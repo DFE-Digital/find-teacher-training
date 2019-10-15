1fff074b327a72616f347081d7526b4cfca06854
dankmitchell <dan@danmitchell.co.uk>
2019-10-15 14:52:11 +0100

    Add static pages and update footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   get "/pages/:page", to: "pages#show"
 
+  get "/terms-conditions", to: "pages#terms", as: "terms"
+  get "/accessibility", to: "pages#accessibility", as: "accessibility"
+  get "/privacy-policy", to: "pages#privacy", as: "privacy"
+  get "/cookies", to: "pages#cookies", as: "cookies"
+
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -8,4 +8,28 @@ RSpec.feature "View pages", type: :feature do
 
     expect(page).to have_text("Lorem")
   end
+
+  scenario "Navigate to cookies" do
+    visit cookies_path
+
+    expect(page).to have_text("Cookies")
+  end
+
+  scenario "Navigate to privacy" do
+    visit privacy_path
+
+    expect(page).to have_text("Privacy policy")
+  end
+
+  scenario "Navigate to accessibility" do
+    visit accessibility_path
+
+    expect(page).to have_text("Accessibility statement for Find teacher training courses")
+  end
+
+  scenario "Navigate to terms" do
+    visit terms_path
+
+    expect(page).to have_text("Terms and Conditions")
+  end
 end
