28986f67ca1475aabee08c07e8987ddf77ea62f0
Toby Retallick <toby.retallick@gmail.com>
2019-12-04 12:15:32 +0000

    [2361] - Upgrade loofah gem

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 class CoursesController < ApplicationController
   def show
     @course = Course
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,25 +1,15 @@
 class Base < JsonApiClient::Resource
-  # This connection class grabs the bearer token from the thread key-value
-  # store and adds it to our connection. This token was saved there for us when
-  # the user was first authenticated.
   class MCBConnection < JsonApiClient::Connection
     def run(request_method, path, params: nil, headers: {}, body: nil)
-      # authorization = "Bearer #{Thread.current.fetch(:manage_courses_backend_token)}"
       super(
         request_method,
         path,
         params: params,
-        # headers: headers.update(
-        #   "Authorization" => authorization,
-        #   "X-Request-Id" => RequestStore.store[:request_id],
-        # ),
         body: body
       )
     end
   end
-
-  # include Draper::Decoratable
-
+  
   self.site = "#{Settings.manage_backend.base_url}/api/v3/"
   self.connection_class = MCBConnection
 
