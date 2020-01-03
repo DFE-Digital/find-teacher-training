455d777fedc78839885be22ac9416ad7d3562613
dankmitchell <dan@danmitchell.co.uk>
2020-01-03 16:08:11 +0000

    [2744] Add cta for dfe apply

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,9 @@
 class Provider < Base
   belongs_to :recruitment_cycle, param: :recruitment_cycle_year
   has_many :courses, param: :course_code
+
+  def supporting_dfe_apply?
+    opted_in_providers = %w[R55 1N1 S31 24L 1HQ L06 2LR 254 12K 1OT]
+    opted_in_providers.include?(provider_code)
+  end
 end
