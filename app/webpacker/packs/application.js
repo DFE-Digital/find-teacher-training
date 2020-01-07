f89743153c53463f1fe316e3f438f695914b4faa
dankmitchell <dan@danmitchell.co.uk>
2020-01-07 21:32:03 +0000

    [2746] Add back to search results link

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,7 +1,11 @@
 import "../styles/application.scss";
 import { initAll } from "govuk-frontend";
 import initLocationsMap from "scripts/locations-map";
+import backLink from "scripts/back-link";
 
 initAll();
 
 window.initLocationsMap = initLocationsMap;
+
+const $backLink = document.querySelector('[data-module="back-link"]');
+new backLink($backLink).init();
