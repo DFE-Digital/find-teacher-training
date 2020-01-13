001a6dc93608e220b11d4800589ccf9dfde296d9
Theodor Vararu <theo@vararu.org>
2020-01-13 16:04:25 +0000

    Send more providers to the Apply service
    :shipit: https://github.com/DFE-Digital/search-and-compare-ui/pull/483

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,7 +3,8 @@ class Provider < Base
   has_many :courses, param: :course_code
 
   def supporting_dfe_apply?
-    opted_in_providers = %w[R55 1N1 S31 24L 1HQ L06 2LR 254 12K 1OT]
+    opted_in_providers = %w[R55 1N1 S31 24L 1HQ L06 2LR 254 12K 1OT N83 2B2 255 2ET T25 1OS S17 C76 2DB M82 2B5 2GU 1ZN]
+
     opted_in_providers.include?(provider_code)
   end
 end
