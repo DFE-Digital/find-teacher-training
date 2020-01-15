6388f954ba1e5fa25668698c7d4a3f02dd4eacba
David Gisbey <davidgisbey@gmail.com>
2020-01-15 16:31:33 +0000

    Send all applicants to Apply
    Apply now has page that points the appropriate users to
    the apply service.

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,13 +1,2 @@
 describe Provider do
-  context "#supporting_dfe_apply?" do
-    it "is an opted-in provider" do
-      provider = build(:provider, provider_code: "R55")
-      expect(provider.supporting_dfe_apply?).to eq(true)
-    end
-
-    it "is not an opted-in provider" do
-      provider = build(:provider, provider_code: "122")
-      expect(provider.supporting_dfe_apply?).to eq(false)
-    end
-  end
 end
