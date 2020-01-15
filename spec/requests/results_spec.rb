93152a9c6dafce081bc9d7c78141868ee909a4ec
Graham Pengelly <graham@three-tier.com>
2020-01-15 12:26:52 +0000

    [2345] Redirect /results to C# app
    Redirect all requests to `ResultsController#index` to C# version for
    now.
    
    Don't redirect in test.

---
+++ spec/requests/results_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "/results", type: :request do
+  before do
+    allow(Settings).to receive(:redirect_results_to_c_sharp).and_return(true)
+  end
+
+  it "redirects to c sharp version" do
+    get "/results"
+    expect(response).to redirect_to(Settings.search_and_compare_ui.base_url + "/results")
+  end
+
+  it "forwards querystring params" do
+    get "/results?test=one&test_two=%26booyah"
+    expect(response).to redirect_to(Settings.search_and_compare_ui.base_url + "/results?test=one&test_two=%26booyah")
+  end
+end
