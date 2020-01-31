a6f29084bf4c14090efa86c48181e5e64e1506f5
defong <de_fong2k@yahoo.com>
2020-01-31 16:56:09 +0000

    [2781] Added vacancies filter

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -16,10 +16,16 @@ feature "results", type: :feature do
       expect(results_page.study_type_filter.parttime).to have_content("Part time (18 - 24 months)")
       expect(results_page.study_type_filter.link).to have_content("Change study type")
     end
+
+    it "has vacancies filter" do
+      expect(results_page.vacancies_filter.subheading).to have_content("Vacancies:")
+      expect(results_page.vacancies_filter.vacancies).to have_content("Only courses with vacancies")
+      expect(results_page.vacancies_filter.link).to have_content("Change vacancies")
+    end
   end
 
   describe "filters defaults with query string" do
-    let(:params) { { fulltime: "False", parttime: "False" } }
+    let(:params) { { fulltime: "False", parttime: "False", hasvacancies: "True" } }
 
     it "has study type filter" do
       expect(results_page.study_type_filter.subheading).to have_content("Study type:")
@@ -27,27 +33,59 @@ feature "results", type: :feature do
       expect(results_page.study_type_filter.parttime).to have_content("Part time (18 - 24 months)")
       expect(results_page.study_type_filter.link).to have_content("Change study type")
     end
-  end
 
-  describe "filters with query string" do
-    let(:params) { { fulltime: "True", parttime: "False" } }
-
-    it "has study type filter for full time only" do
-      expect(results_page.study_type_filter.subheading).to have_content("Study type:")
-      expect(results_page.study_type_filter.fulltime).to have_content("Full time (12 months)")
-      expect(results_page.study_type_filter).not_to have_parttime
-      expect(results_page.study_type_filter.link).to have_content("Change study type")
+    it "has vacancies filter" do
+      expect(results_page.vacancies_filter.subheading).to have_content("Vacancies:")
+      expect(results_page.vacancies_filter.vacancies).to have_content("Only courses with vacancies")
+      expect(results_page.vacancies_filter.link).to have_content("Change vacancies")
     end
   end
 
   describe "filters with query string" do
-    let(:params) { { fulltime: "False", parttime: "True" } }
+    describe "study type filter" do
+      context "for full time only" do
+        let(:params) { { fulltime: "True", parttime: "False" } }
 
-    it "has study type filter for part time only" do
-      expect(results_page.study_type_filter.subheading).to have_content("Study type:")
-      expect(results_page.study_type_filter).not_to have_fulltime
-      expect(results_page.study_type_filter.parttime).to have_content("Part time (18 - 24 months)")
-      expect(results_page.study_type_filter.link).to have_content("Change study type")
+        it "has study type filter for full time only " do
+          expect(results_page.study_type_filter.subheading).to have_content("Study type:")
+          expect(results_page.study_type_filter.fulltime).to have_content("Full time (12 months)")
+          expect(results_page.study_type_filter).not_to have_parttime
+          expect(results_page.study_type_filter.link).to have_content("Change study type")
+        end
+      end
+
+      context "for part time only" do
+        let(:params) { { fulltime: "False", parttime: "True" } }
+
+        it "has study type filter for part time only" do
+          expect(results_page.study_type_filter.subheading).to have_content("Study type:")
+          expect(results_page.study_type_filter).not_to have_fulltime
+          expect(results_page.study_type_filter.parttime).to have_content("Part time (18 - 24 months)")
+          expect(results_page.study_type_filter.link).to have_content("Change study type")
+        end
+      end
+    end
+
+    describe "vacancies filter" do
+      context "only courses with vacancies" do
+        let(:params) { { hasvacancies: "True" } }
+
+        it "has vacancies filter" do
+          expect(results_page.vacancies_filter.subheading).to have_content("Vacancies:")
+          expect(results_page.vacancies_filter.vacancies).to have_content("Only courses with vacancies")
+          expect(results_page.vacancies_filter.link).to have_content("Change vacancies")
+        end
+      end
+
+      context "courses with and without vacancies" do
+        let(:params) { { hasvacancies: "False" } }
+
+        it "has vacancies filter" do
+          expect(results_page.vacancies_filter.subheading).to have_content("Vacancies:")
+          expect(results_page.vacancies_filter.vacancies).to have_content("Courses with and without vacancies")
+          expect(results_page.vacancies_filter.link).to have_content("Change vacancies")
+        end
+      end
     end
   end
 end
