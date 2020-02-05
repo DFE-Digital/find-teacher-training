1e18eabc4642a9a979ed7de4e45dffad56727f0f
Toby Retallick <toby.retallick@gmail.com>
2020-02-05 12:04:45 +0000

    [2783] - Extract params helpers, errors and validation logic to a form object

---
+++ spec/form_objects/result_filters/location_filter_form_spec.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+module ResultFilters
+  describe LocationFilterForm do
+    before do
+      stub_geocoder
+    end
+
+    describe "Validation" do
+      subject { described_class.new(params) }
+      context "no option selected" do
+        let(:params) { {} }
+        it "is not valid" do
+          expect(subject.valid?).to eq(false)
+        end
+
+        it "has error" do
+          subject.valid?
+          expect(subject.errors).to eq("Please choose an option")
+        end
+
+        it "has empty params" do
+          subject.valid?
+          expect(subject.params).to be_empty
+        end
+      end
+
+      context "unknown location" do
+        let(:params) { { l: "1", lq: "Unknown location" } }
+
+        it "is not valid" do
+          expect(subject.valid?).to eq(false)
+        end
+
+        it "has error" do
+          subject.valid?
+          expect(subject.errors).to eq("We couldn't find this location, please check your input and try again")
+        end
+
+        it "has params" do
+          subject.valid?
+          expect(subject.params).to eq(params)
+        end
+      end
+
+      context "known location" do
+        let(:params) { { l: "1", lq: "SW1P 3BT" } }
+
+        it "is not valid" do
+          expect(subject.valid?).to eq(true)
+        end
+
+        it "has no errors" do
+          subject.valid?
+          expect(subject.errors).to be_nil
+        end
+
+        it "has params" do
+          subject.valid?
+          expect(subject.params).to eq(params)
+        end
+      end
+    end
+  end
+end
