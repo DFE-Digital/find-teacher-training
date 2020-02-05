b1f9edda07a03047e9a262df35d8bc52c5b418ca
defong <de_fong2k@yahoo.com>
2020-02-05 17:13:32 +0000

    [2901] Fixed backlink for study type

--- app/controllers/result_filters/study_type_controller.rb
+++ app/controllers/result_filters/study_type_controller.rb
@@ -2,6 +2,8 @@ module ResultFilters
   class StudyTypeController < ApplicationController
     include FilterParameters
 
+    before_action :build_results_filter_query_parameters
+
     def new; end
 
     def create
@@ -11,6 +13,13 @@ module ResultFilters
         flash[:error] = "You must make at least one selection"
         redirect_to studytype_path(filter_params)
       end
+    end
+
+  private
+
+    def build_results_filter_query_parameters
+      @results_filter_query_parameters = ResultsView.new(query_parameters: request.query_parameters)
+        .query_parameters_with_defaults
     end
   end
 end
