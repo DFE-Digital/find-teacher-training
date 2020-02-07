b33a0126bff16e642abeaf30a0956c77e1c1c03c
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-02-07 13:46:37 +0000

    [2779] Extract subjects params into a seperate method
    to imporve readability, which was raised during the review.
    
     Please enter the commit message for your changes. Lines starting

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -34,7 +34,7 @@ class ResultsController < ApplicationController
       if params["subjects"].present?
         SubjectArea.includes(:subjects).all
           .map(&:subjects).flatten
-          .select { |subject| rails_to_csharp_subject_id(id: subject.id).in?(params["subjects"].split(",").map(&:to_s)) }
+          .select { |subject| rails_to_csharp_subject_id(id: subject.id).in?(subjects_params_array) }
           .sort_by(&:subject_name)[0..3]
       else
         SubjectArea.includes(:subjects).all
@@ -56,5 +56,9 @@ private
     redirect_uri.query = query_string.gsub("%2C", ",") if query_string.present?
 
     redirect_to redirect_uri.to_s
+  end
+
+  def subjects_params_array
+    params["subjects"].split(",")
   end
 end
