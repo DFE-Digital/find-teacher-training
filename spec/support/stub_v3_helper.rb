55eebf14a70e34386e58e6a879989668a2849d36
dankmitchell <dan@danmitchell.co.uk>
2020-02-17 09:03:36 +0000

    Update settings from manage_backend to api

--- spec/support/stub_v3_helper.rb
+++ spec/support/stub_v3_helper.rb
@@ -1,6 +1,6 @@
 module StubV3Helper
   def stub_api_v3_request(url_path, stub, method = :get, status = 200)
-    url = "#{Settings.manage_backend.base_url}/api/v3#{url_path}"
+    url = "#{Settings.teacher_training_api.base_url}/api/v3#{url_path}"
 
     stubbed_request = stub_request(method, url)
                         .to_return(
