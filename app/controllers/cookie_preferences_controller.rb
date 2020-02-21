4d4f4444d88ef86600260e4a78fc5c0cf6cde296
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-02-21 14:57:20 +0000

    [2968] Validate creating cookie preferences
    and show flash messages

--- app/controllers/cookie_preferences_controller.rb
+++ app/controllers/cookie_preferences_controller.rb
@@ -2,8 +2,15 @@ class CookiePreferencesController < ApplicationController
   def new; end
 
   def create
+    if params[:cookie_consent].blank?
+      flash[:error] = I18n.t("cookie_preferences.no_option_error")
+      redirect_to(cookie_preferences_path)
+    else
       user_preference = params[:cookie_consent]
       cookies["consented-to-cookies"] = { value: user_preference, expires: 6.months.from_now }
+
+      flash[:success] = I18n.t("cookie_preferences.success")
       redirect_back(fallback_location: root_path)
+    end
   end
 end
--- spec/features/cookie_consent_spec.rb
+++ spec/features/cookie_consent_spec.rb
@@ -8,4 +8,27 @@ RSpec.feature "Cookie consent", type: :feature do
 
     expect(page).to have_text("Cookies")
   end
+
+  scenario "Consent to cookies" do
+    visit cookie_preferences_path
+    page.choose("Yes, opt-in to Google Analytic cookies")
+    click_button "Save changes"
+
+    expect(page).to have_text(I18n.t("cookie_preferences.success"))
+  end
+
+  scenario "Does not consent to cookies" do
+    visit cookie_preferences_path
+    page.choose("No, do not track my website usage")
+    click_button "Save changes"
+
+    expect(page).to have_text(I18n.t("cookie_preferences.success"))
+  end
+
+  scenario "No cookie preference selected" do
+    visit cookie_preferences_path
+    click_button "Save changes"
+
+    expect(page).to have_text(I18n.t("cookie_preferences.no_option_error"))
+  end
 end
