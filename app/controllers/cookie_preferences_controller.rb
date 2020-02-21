3b57962d20e4d928a6f499b85f95f25fc35aed62
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-02-21 14:51:49 +0000

    [2968] Add create action to the cookie_preferences controller

--- app/controllers/cookie_preferences_controller.rb
+++ app/controllers/cookie_preferences_controller.rb
@@ -2,5 +2,8 @@ class CookiePreferencesController < ApplicationController
   def new; end
 
   def create
+      user_preference = params[:cookie_consent]
+      cookies["consented-to-cookies"] = { value: user_preference, expires: 6.months.from_now }
+      redirect_back(fallback_location: root_path)
   end
 end
--- spec/controllers/cookie_preferences_controller_spec.rb
+++ spec/controllers/cookie_preferences_controller_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe CookiePreferencesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "#create" do
+    it "sets cookie value" do
+      post :create, params: { cookie_consent: "true" }
+
+      expect(response.cookies["consented-to-cookies"]).to eq("true")
+    end
+
+    it "sets cookie expiry" do
+      stub_cookie_jar = HashWithIndifferentAccess.new
+      allow(controller).to receive(:cookies).and_return(stub_cookie_jar)
+
+      post :create, params: { cookie_consent: "true" }
+      cookie = stub_cookie_jar["consented-to-cookies"]
+
+      expect(cookie[:expires]).to be_within(1.second).of(6.months.from_now)
+    end
+  end
 end
