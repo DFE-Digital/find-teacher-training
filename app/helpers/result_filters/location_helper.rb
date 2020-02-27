9d3b53934c328be1637165c039e5fd42ebf54408
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-02-27 12:00:07 +0000

    [3059] Make predicate helper methods consistent and add unit tests
    This make the location filter methods return `false` instead of `nil` when no
    error appears.

--- app/helpers/result_filters/location_helper.rb
+++ app/helpers/result_filters/location_helper.rb
@@ -1,15 +1,21 @@
 module ResultFilters
   module LocationHelper
     def provider_error?
-      flash[:error] && flash[:error].include?(I18n.t("location_filter.fields.provider"))
+      return false if flash[:error].nil?
+
+      flash[:error].include?(I18n.t("location_filter.fields.provider"))
     end
 
     def location_error?
-      flash[:error] && flash[:error].include?(I18n.t("location_filter.fields.location"))
+      return false if flash[:error].nil?
+
+      flash[:error].include?(I18n.t("location_filter.fields.location"))
     end
 
     def no_option_selected?
-      flash[:error] && flash[:error].include?(I18n.t("location_filter.errors.no_option"))
+      return false if flash[:error].nil?
+
+      flash[:error].include?(I18n.t("location_filter.errors.no_option"))
     end
   end
 end
