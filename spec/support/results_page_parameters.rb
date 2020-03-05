f7da85d0ca9e31c7e9ad1f9e601494abf97695e9
dankmitchell <dan@danmitchell.co.uk>
2020-03-05 22:23:26 +0000

    [3085] Fix funding option on results page

--- spec/support/results_page_parameters.rb
+++ spec/support/results_page_parameters.rb
@@ -2,7 +2,7 @@ def results_page_parameters(parameters = {})
   {
     "filter[vacancies]" => "true",
     "filter[qualifications]" => "QtsOnly,PgdePgceWithQts,Other",
-    "include" => "provider,sites",
+    "include" => "provider,sites,subjects",
     "page[page]" => 1,
     "page[per_page]" => 10,
     "sort" => "provider.provider_name,name",
