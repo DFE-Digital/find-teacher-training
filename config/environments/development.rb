ae646265d42e309a9146876041f281ddf1db3bb2
Toby Retallick <toby.retallick@gmail.com>
2020-03-09 16:25:59 +0000

    [3088] - add config per environment for 'rails semantic logger' and 'logstash'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # logging
+  config.log_level = Settings.log_level
+
+  config.logger = ActiveSupport::Logger.new(STDOUT)
 end
