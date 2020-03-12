2e5a2fcfa8bc85270f987447ff1834664bf4773e
Toby Retallick <toby.retallick@gmail.com>
2020-03-12 12:51:39 +0000

    [3120] - Encode the provider suggestion query
    - This will prevent the JSONApiClient raising a 'URI::InvalidURIError'
    if the query contains non-ASCII characters

--- spec/requests/provider_suggestions_spec.rb
+++ spec/requests/provider_suggestions_spec.rb
@@ -17,36 +17,35 @@ describe "/provider-suggestions", type: :request do
       expect(JSON.parse(response.body)).to eq("error" => "Bad request")
     end
   end
-  context "when provider suggestion ids valid" do
-    let(:provider_suggestions) {
-      stub_request(:get, "#{Settings.teacher_training_api.base_url}/api/v3/provider-suggestions?query=Str")
-        .to_return(
-          headers: { "Content-Type": "application/vnd.api+json; charset=utf-8" },
-          body: File.new("spec/fixtures/api_responses/provider-suggestions.json"),
-        )
-    }
+  context "when provider suggestion query is valid" do
+    query = "Str"
+    query_with_encoded_characters = "Str%E2%80%99"
 
-    before do
-      provider_suggestions
-    end
+    [query, query_with_encoded_characters].each do |provider_query|
+      it "returns success (200) for query: '#{provider_query}'" do
+        provider_suggestions = stub_request(:get, "#{Settings.teacher_training_api.base_url}/api/v3/provider-suggestions?query=#{provider_query}")
+                                 .to_return(
+                                   headers: { "Content-Type": "application/vnd.api+json; charset=utf-8" },
+                                   body: File.new("spec/fixtures/api_responses/provider-suggestions.json"),
+                                 )
 
-    it "returns success (200)" do
-      get "/provider-suggestions?query=Str"
+        get "/provider-suggestions?query=#{provider_query}"
 
-      expect(provider_suggestions).to have_been_requested
-      expect(response.status).to eq(200)
-      expect(JSON.parse(response.body)).to eq(
-        [
-           {
-             "code" => "A0",
-             "name" => "ACME SCITT 0",
-           },
-           {
-             "code" => "A01",
-             "name" => "Acme SCITT",
-           },
-         ],
-       )
+        expect(provider_suggestions).to have_been_requested
+        expect(response.status).to eq(200)
+        expect(JSON.parse(response.body)).to eq(
+          [
+            {
+              "code" => "A0",
+              "name" => "ACME SCITT 0",
+            },
+            {
+              "code" => "A01",
+              "name" => "Acme SCITT",
+            },
+          ],
+         )
+      end
     end
   end
 end
