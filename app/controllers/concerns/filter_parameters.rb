e03b0fd197e70942caa54720809ec851b4b20585
Graham Pengelly <graham@three-tier.com>
2020-03-23 07:05:38 +0000

    [3125] Add previous parameters to FilterParameters
    * Add support for `prev_` prefixed parameters to the `FilterParameters`
    concern
    * Add tests for existing functionality
    
    The `prev_` prefixed parameters are used for preserving an original set
    of filters through the creation of a new set to allow the user to return
    to the original filter after a validation failure. This adds methods
    for merging and removing them to the `FilterParameters` concern.

--- app/controllers/concerns/filter_parameters.rb
+++ app/controllers/concerns/filter_parameters.rb
@@ -1,11 +1,7 @@
 module FilterParameters
-  def filter_params
-    if request.method == "GET"
-      parameters = request.query_parameters
-    elsif request.method == "POST"
-      parameters = request.request_parameters
-    end
+  PREVIOUS_PARAMETER_PREFIX = "prev_".freeze
 
+  def filter_params
     custom_params = parameters.reject do |param|
       param.in? %w(utf8 authenticity_token)
     end
@@ -25,7 +21,37 @@ module FilterParameters
     params[param].split(",") if params.key?(param)
   end
 
+  def filter_params_without_previous_parameters
+    remove_previous_parameters(filter_params)
+  end
+
+  def merge_previous_parameters(all_parameters)
+    previous_parameters.each do |key, value|
+      next if value == "none"
+
+      all_parameters[key.delete_prefix(PREVIOUS_PARAMETER_PREFIX)] = value
+    end
+
+    remove_previous_parameters(all_parameters)
+  end
+
+private
+
   def serialize_array_filter_param(value)
     value.join(",") if value.present?
+  end
+
+  def parameters
+    return request.query_parameters if request.method == "GET"
+
+    request.request_parameters
+  end
+
+  def previous_parameters
+    parameters.select { |key, _value| key.start_with? PREVIOUS_PARAMETER_PREFIX }
+  end
+
+  def remove_previous_parameters(all_parameters)
+    all_parameters.reject { |key, _value| key.start_with? PREVIOUS_PARAMETER_PREFIX }
   end
 end
---
+++ spec/controllers/concerns/filter_parameters_spec.rb
@@ -0,0 +1,89 @@
+require_relative "../../../app/controllers/concerns/filter_parameters"
+require "active_support/core_ext"
+
+RSpec.describe FilterParameters do
+  class TestClass
+    include FilterParameters
+
+    attr_reader :request
+
+    def initialize(request:)
+      @request = request
+    end
+
+    def params
+      request.query_parameters
+    end
+  end
+
+  let(:request) do
+    double(method: verb, query_parameters: query_parameters, request_parameters: request_parameters)
+  end
+  let(:query_parameters) { {} }
+  let(:request_parameters) { {} }
+  let(:verb) { "GET" }
+
+  subject do
+    TestClass.new(request: request)
+  end
+
+  describe "#filter_params" do
+    context "GET" do
+      context "when rails parameters are present" do
+        let(:query_parameters) { { "utf8" => true, "authenticity_token" => "token", "test" => "test" } }
+
+        it "they are stripped" do
+          expect(subject.filter_params).to eq({ "test" => "test" })
+        end
+      end
+
+      context "when an array parameter is present" do
+        let(:query_parameters) { { "test" => [1, 2, 3] } }
+
+        it "serializes them to a comma separated string" do
+          expect(subject.filter_params["test"]).to eq("1,2,3")
+        end
+      end
+    end
+
+    context "POST" do
+      let(:verb) { "POST" }
+      let(:request_parameters) { { "test" => "request" } }
+      let(:query_parameters) { { "test" => "query" } }
+
+      it "uses request_parameters" do
+        expect(subject.filter_params["test"]).to eq("request")
+      end
+    end
+  end
+
+  describe "#deserialize_array_filter_param" do
+    let(:query_parameters) { { "test" => "1,2,3" } }
+    it "splits comma separated strings" do
+      expect(subject.deserialize_array_filter_param("test")).to match_array(%w(1 2 3))
+    end
+  end
+
+  describe "#filter_params_without_previous_parameters" do
+    let(:query_parameters) { { "l" => 0, "prev_l" => 1, "prev_query" => "query", "test" => "test" } }
+
+    it "removes the previous parameters" do
+      expect(subject.filter_params_without_previous_parameters).to eq({ "l" => 0, "test" => "test" })
+    end
+  end
+
+  describe "#merge_previous_parameters" do
+    let(:query_parameters) { { "l" => 0, "prev_l" => 1, "prev_query" => "query", "test" => "test" } }
+
+    it "sets the non prev equivalent and adds any that are not present" do
+      expect(subject.merge_previous_parameters(query_parameters)).to eq({ "l" => 1, "query" => "query", "test" => "test" })
+    end
+
+    context "where param is set to none" do
+      let(:query_parameters) { { "l" => 0, "prev_l" => 1, "prev_query" => "query", "test" => "test", "prev_lat" => "none" } }
+      it "ignores them" do
+        expect(subject.merge_previous_parameters(query_parameters)).to eq({ "l" => 1, "query" => "query", "test" => "test" })
+      end
+    end
+  end
+end
