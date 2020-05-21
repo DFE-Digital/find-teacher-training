8668261ace72a47b85a8d76d2153cb3acccd254f
dankmitchell <dan@danmitchell.co.uk>
2020-05-21 13:55:34 +0100

    [3427] Udpate apply link for bolton uni courses

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -25,6 +25,16 @@ RSpec.describe CoursesController do
       expect(response).to redirect_to("https://www.apply-for-teacher-training.education.gov.uk/candidate/apply?providerCode=#{course.provider.provider_code}&courseCode=#{course.course_code}")
     end
 
+    context "a course from University of Bolton" do
+      let(:provider) { build(:provider, provider_code: "8N5") }
+      let(:course) { build(:course, course_code: "V595", provider: provider) }
+
+      it "redirects to University of Bolton's applications service" do
+        get :apply, params: { provider_code: course.provider_code, course_code: course.course_code }
+        expect(response).to redirect_to("https://evision.bolton.ac.uk/urd/sits.urd/run/siw_ipp_lgn.login?process=siw_ipp_app&code1=1101-D&code2=0001")
+      end
+    end
+
     it "writes to log" do
       allow(Rails).to receive(:logger).and_return(logger)
       expect(logger).to receive(:info).with("Course apply conversion. Provider: #{course.provider.provider_code}. Course: #{course.course_code}")
