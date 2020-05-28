576c054e656d71ab70cb00ad446a6eb5478e2f32
Toby Retallick <toby.retallick@gmail.com>
2020-05-28 12:00:43 +0100

    [3434] - Location filter provider search with one character
    - An error is displayed if a user tries to search for a provider with a
    string containing only one character
    - Correct error message copy

--- app/controllers/result_filters/provider_controller.rb
+++ app/controllers/result_filters/provider_controller.rb
@@ -4,7 +4,12 @@ module ResultFilters
 
     def new
       if params[:query].blank?
-        flash[:error] = [I18n.t("location_filter.fields.provider")]
+        flash[:error] = [t("location_filter.fields.provider"), t("location_filter.errors.blank_provider")]
+        return redirect_back
+      end
+
+      if params[:query].length == 1
+        flash[:error] = [t("location_filter.fields.provider"), t("location_filter.errors.invalid_provider")]
         return redirect_back
       end
 
@@ -15,7 +20,7 @@ module ResultFilters
         .all
 
       if @provider_suggestions.count.zero?
-        flash[:error] = [I18n.t("location_filter.fields.provider")]
+        flash[:error] = [I18n.t("location_filter.fields.provider"), I18n.t("location_filter.errors.missing_provider")]
         redirect_back
       elsif @provider_suggestions.count == 1
         redirect_to results_path(
