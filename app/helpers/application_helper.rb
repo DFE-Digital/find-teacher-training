43a0f0befb330920af8501e9a403f0e406e51017
Paul Hayes <fofr@users.noreply.github.com>
2020-06-08 10:57:33 +0100

    Don’t use HTML entities for smart quotes
    Use characters rather than HTML entities for smart quotes this matches how we write smart quotes in templates and allows us to use them in <title> elements
    
    https://github.com/jmcnevin/rubypants/blob/master/lib/rubypants.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,29 @@ module ApplicationHelper
     # autolink: parse links even when they are not enclosed in <> characters
     options = { autolink: true, lax_spacing: true }
     markdown = Redcarpet::Markdown.new(render, options)
-    source_with_smart_quotes = RubyPants.new(source).to_html
+    source_with_smart_quotes = smart_quotes(source)
     markdown.render(source_with_smart_quotes).html_safe
   end
 
   def smart_quotes(string)
-    RubyPants.new(string).to_html.html_safe
+    RubyPants.new(string, 2, ruby_pants_options).to_html
+  end
+
+private
+
+  # Use characters rather than HTML entities for smart quotes this matches how
+  # we write smart quotes in templates and allows us to use them in <title>
+  # elements
+  # https://github.com/jmcnevin/rubypants/blob/master/lib/rubypants.rb
+  def ruby_pants_options
+    {
+      double_left_quote: "“",
+      double_right_quote: "”",
+      single_left_quote: "‘",
+      single_right_quote: "’",
+      ellipsis: "…",
+      em_dash: "—",
+      en_dash: "–",
+    }
   end
 end
