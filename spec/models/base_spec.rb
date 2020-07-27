1c3a0713fd8d53f359c4f6ce9a6e16ab025bf426
Phil Lee <asmega@gmail.com>
2020-07-27 16:16:42 +0100

    [3684] Add X-Reqest-Id to API client connection
    - Previous connection_class was not correctly set therefore the custom
    request_id header was never sent

---
+++ spec/models/base_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe Base do
+  describe "connection_class" do
+    before do
+      RequestStore.store[:request_id] = "some-request-id"
+    end
+
+    it "connection sends X-Request-Id header" do
+      stub = stub_request(:post, "http://localhost:3001/api/v3/bases")
+        .with(
+          body: "{\"data\":{\"type\":\"bases\",\"attributes\":{}}}",
+          headers: {
+            "X-Request-Id" => "some-request-id",
+          },
+        ).to_return(status: 200, body: "", headers: {})
+
+      Base.new.save
+
+      expect(stub).to have_been_requested
+    end
+  end
+end
