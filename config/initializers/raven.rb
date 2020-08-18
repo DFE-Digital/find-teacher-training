07b89a738ba3185dc8d7c5f4340f933529a4703b
Tijmen Brommet <tijmen@gmail.com>
2020-08-18 08:16:53 +0100

    Don't send unactionable errors to Sentry
    `JsonApiClient::Errors::ConnectionError` is caused by the API being
    down or unreachable. This doesn't require an action from developers. To
    avoid error blindness and reduce the number of messages in the Slack
    channel, this filters out all the errors. Our existing monitoring will
    pick up if the site is down for an extended period of time.

---
+++ config/initializers/raven.rb
@@ -0,0 +1,14 @@
+# https://docs.sentry.io/clients/ruby/config
+Raven.configure do |config|
+  # > Inspect an incoming exception's causes when determining whether or not that
+  # exception should be excluded
+  #
+  # This will also exclude exceptions like `ActionView::Template::Error` if
+  # they're caused by `JsonApiClient::Errors::ConnectionError`, like
+  # https://sentry.io/organizations/dfe-bat/issues/1845349352
+  config.inspect_exception_causes_for_exclusion = true
+
+  config.excluded_exceptions += [
+    "JsonApiClient::Errors::ConnectionError",
+  ]
+end
