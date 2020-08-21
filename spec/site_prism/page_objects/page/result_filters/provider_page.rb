c7b853f8416f62ba42429f5b655fdd35cb51854c
Theodor Vararu <theo@vararu.org>
2020-08-21 16:05:03 +0100

    Fix invalid HTML on Pick a provider page
    - Closes issue #288 - https://github.com/DFE-Digital/find-teacher-training/issues/288
    - Remove 'data-qa' attributes (as part of an agreed effort to only use
    'data-qa' attributes only when absolutely necessary)
    
    Co-authored-by: Theodor Vararu <theo@vararu.org>
    Co-authored-by: Toby Retallick <toby.retallick@gmail.com>

--- spec/site_prism/page_objects/page/result_filters/provider_page.rb
+++ spec/site_prism/page_objects/page/result_filters/provider_page.rb
@@ -12,11 +12,10 @@ module PageObjects
         element :back_link, '[data-qa="page-back"]'
         element :error, '[data-qa="error"]'
 
-        section :search, '[data-qa="search"]' do
-          element :input, '[data-qa="search__input"]'
-          element :expand, '[data-qa="search__expand"]'
-          element :submit, '[data-qa="search__submit"]'
-        end
+        element :search_expand, "details"
+        element :search_input, "details .govuk-input"
+        element :search_submit, "details .govuk-button"
+
         sections :provider_suggestions, ProviderSuggestion, '[data-qa="provider_suggestion"]'
       end
     end
