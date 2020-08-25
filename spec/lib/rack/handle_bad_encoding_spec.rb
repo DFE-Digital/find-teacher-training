6f3fd1cf38babdefdcd1a0881335de844978d123
Toby Retallick <toby.retallick@gmail.com>
2020-08-25 11:43:19 +0100

    Redirect to 422 page when query is invalid
    - Previously we were sanitizing an invalid query, returning all results.
    Now we are redirecting any requests with invalid queries to a 422 page.

--- spec/lib/rack/handle_bad_encoding_spec.rb
+++ spec/lib/rack/handle_bad_encoding_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
 describe Rack::HandleBadEncoding do
+  include Rack::Test::Methods
+
   let(:app) { double }
   let(:middleware) { described_class.new(app) }
 
-  %w[/location-suggestions /provider-suggestions].each do |path|
+  %w[/location-suggestions /provider-suggestions /results].each do |path|
     context "request path is #{path}" do
       context "query does not contain invalid encodings" do
         it "does not modify the query" do
@@ -24,21 +26,21 @@ describe Rack::HandleBadEncoding do
       end
 
       context "query contains invalid encodings" do
-        it "modifies the query" do
-          expect(app).to receive(:call).with(
-            "QUERY_STRING" => "",
-            "REQUEST_PATH" => path,
-          )
-          middleware.call(
+        it "redirects to 422 page" do
+          expect(app).to_not receive(:call)
+
+          request = middleware.call(
             "QUERY_STRING" => "query=%2Flondon%2bot%Forder%3Ddescending%26page%3D5%26sort%3Dcreated_at",
             "REQUEST_PATH" => path,
           )
+
+          expect(request).to eq([301, { "Location" => "/422", "Content-Type" => "text/html" }, []])
         end
       end
     end
   end
 
-  context "request path is not 'location-suggestions' or '/provider-suggestions'" do
+  context "request path is not 'location-suggestions', '/provider-suggestions' or '/results'" do
     it "does not modify the query" do
       expect(app).to receive(:call).with(
         "QUERY_STRING" => "query=%2Flondon%2bot%Forder%3Ddescending%26page%3D5%26sort%3Dcreated_at",
