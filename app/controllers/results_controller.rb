21dbd23d9074aa14179f03ebcf7382902ffeb672
Toby Retallick <toby.retallick@gmail.com>
2020-08-28 10:47:07 +0100

    Redirect to 422 page when user requests non existent results page
    - As a result of the End of Cycle event, there are fewer courses
    currently available. Users may have bookmarked links to results pages
    that do not exist.
    - If a user requests a results page that no longer exists, we now direct
    them to a 422 page.
    - This is a short term fix. In the medium term we should refactor the
    ResultsView and separate out the logic for calling the Teacher
    Training API

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -6,5 +6,12 @@ class ResultsController < ApplicationController
     end
 
     @results_view = ResultsView.new(query_parameters: request.query_parameters)
+
+    begin
+      @courses = @results_view.courses.all
+      @number_of_courses_string = @results_view.number_of_courses_string
+    rescue JsonApiClient::Errors::ClientError
+      render template: "errors/unprocessable_entity", status: :unprocessable_entity
+    end
   end
 end
