2ca7cf3b9fd47c06b7997b31ffbb5b620348b7dc
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-10-07 10:47:34 +0100

    Configure Logstash gem only when not running in PaaS
    LOGSTASH_HOST & LOGSTASH_PORT is not configured when running in PaaS.

--- config/initializers/logstash.rb
+++ config/initializers/logstash.rb
@@ -1,6 +1,8 @@
-LogStashLogger.configure do |config|
-  config.customize_event do |event|
-    event["application"] = Settings.application_name
-    event["environment"] = Rails.env
+if Settings.logstash.host && Settings.logstash.port
+  LogStashLogger.configure do |config|
+    config.customize_event do |event|
+      event["application"] = Settings.application_name
+      event["environment"] = Rails.env
+    end
   end
 end
