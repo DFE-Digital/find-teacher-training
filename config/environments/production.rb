9c3d95d5641c9e4a8b32a18c551de73e0f737a44
Vignesh.N <vigneshmsft@users.noreply.github.com>
2020-10-26 14:45:36 +0000

    use json log format

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
   if Settings.logstash.host && Settings.logstash.port
     config.logger = LogStashLogger.new(Settings.logstash.to_h)
   else
-    config.semantic_logger.add_appender(io: STDOUT, level: config.log_level, formatter: :json)
+    config.rails_semantic_logger.format = :json
+    config.semantic_logger.add_appender(io: STDOUT, level: config.log_level, formatter: config.rails_semantic_logger.format)
     config.logger = ActiveSupport::Logger.new(STDOUT)
     config.logger.info("Application logging to STDOUT")
   end
