1bf63676ab1b052e48f2b892f2ff20b4e039e0d3
Toby Retallick <toby.retallick@gmail.com>
2020-11-05 14:31:29 +0000

    Display 404 page when candidate attempts to apply to non-existent course

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -4,32 +4,48 @@ RSpec.describe CoursesController do
   let(:provider) { build(:provider) }
   let(:course) { build(:course, provider: provider) }
 
-  before do
-    stub_api_v3_resource(
-      type: Course,
-      params: {
-        recruitment_cycle_year: Settings.current_cycle,
-        provider_code: course.provider_code,
-        course_code: course.course_code,
-      },
-      resources: course,
-      include: %w[provider],
-    )
-  end
-
   describe "#apply" do
-    let(:logger) { double(:logger) }
+    context "when course is found" do
+      let(:logger) { double(:logger) }
 
-    it "redirects to correct apply destination" do
-      get :apply, params: { provider_code: course.provider_code, course_code: course.course_code }
-      expect(response).to redirect_to("https://www.apply-for-teacher-training.service.gov.uk/candidate/apply?providerCode=#{course.provider.provider_code}&courseCode=#{course.course_code}")
+      before do
+        stub_api_v3_resource(
+          type: Course,
+          params: {
+            recruitment_cycle_year: Settings.current_cycle,
+            provider_code: course.provider_code,
+            course_code: course.course_code,
+          },
+          resources: course,
+          include: %w[provider],
+        )
+      end
+
+      it "redirects to correct apply destination" do
+        get :apply, params: { provider_code: course.provider_code, course_code: course.course_code }
+        expect(response).to redirect_to("https://www.apply-for-teacher-training.service.gov.uk/candidate/apply?providerCode=#{course.provider.provider_code}&courseCode=#{course.course_code}")
+      end
+
+      it "writes to log" do
+        allow(Rails).to receive(:logger).and_return(logger)
+        expect(logger).to receive(:info).with("Course apply conversion. Provider: #{course.provider.provider_code}. Course: #{course.course_code}")
+
+        get :apply, params: { provider_code: course.provider_code, course_code: course.course_code }
+      end
     end
 
-    it "writes to log" do
-      allow(Rails).to receive(:logger).and_return(logger)
-      expect(logger).to receive(:info).with("Course apply conversion. Provider: #{course.provider.provider_code}. Course: #{course.course_code}")
+    context "when a course is not found" do
+      before do
+        stub_request(
+          :get,
+          "#{Settings.teacher_training_api.base_url}/api/v3/recruitment_cycles/#{Settings.current_cycle}/providers/#{course.provider_code}/courses/#{course.course_code}?include=provider",
+        ).to_raise(JsonApiClient::Errors::NotFound)
+      end
 
-      get :apply, params: { provider_code: course.provider_code, course_code: course.course_code }
+      it "redirects to the not found page" do
+        get :apply, params: { provider_code: course.provider_code, course_code: course.course_code }
+        expect(response.status).to eq(404)
+      end
     end
   end
 end
