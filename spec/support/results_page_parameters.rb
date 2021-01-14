495c3206e06fb46b7ebbb0f0964259c64ea844d8
Toby Retallick <toby.retallick@gmail.com>
2021-01-14 16:20:47 +0000

    Don't pass in the 'has_vacancies' filter param to the API when requesting courses with OR without vacancies
    Currently when we request courses with or without vacancies (now the default behaviour on Find) we are passing in the filter param `{ has_vacancies: true}`
    
    This is incorrect from in terms of TTAPI specfication - we should just not pass in anything at all as the default behaviour is to return all courses, regardless of whether they have vacancies or not. See https://api.publish-teacher-training-courses.service.gov.uk/api-reference.html#schema-coursesingleresponse

--- spec/support/results_page_parameters.rb
+++ spec/support/results_page_parameters.rb
@@ -1,6 +1,5 @@
 def results_page_parameters(parameters = {})
   {
-    'filter[has_vacancies]' => 'false',
     'include' => 'site_statuses.site,provider,subjects',
     'page[page]' => 1,
     'page[per_page]' => 10,
