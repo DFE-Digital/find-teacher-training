71fbbbf9fb84f1171d2b3826c7e3b781d49c6f5c
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-02-01 19:26:49 +0000

    Use secrets stored in KeyVault
    Move from storing secrets in GitHub to using Azure KeyVault
    
    BAT-INFRA-SECRETS-{ENV} & FIND-APP-SECRETS-{ENV}
    stored in Azure KeyVault

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.29.0"
+      version = "2.45.1"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
@@ -14,30 +14,40 @@ terraform {
       version = "1.0.0"
     }
   }
-  backend "azurerm" {
+  backend azurerm {
   }
+}
+
+provider azurerm {
+  features {}
+
+  skip_provider_registration = true
+  subscription_id            = local.azure_credentials.subscriptionId
+  client_id                  = local.azure_credentials.clientId
+  client_secret              = local.azure_credentials.clientSecret
+  tenant_id                  = local.azure_credentials.tenantId
 }
 
 module paas {
   source = "./modules/paas"
 
   cf_api_url                = local.cf_api_url
-  cf_user                   = var.paas_user
-  cf_user_password          = var.paas_password
+  cf_user                   = local.infra_secrets.CF_USER
+  cf_user_password          = local.infra_secrets.CF_PASSWORD
   cf_sso_passcode           = var.paas_sso_code
   cf_space                  = var.paas_cf_space
   app_environment           = var.paas_app_environment
   app_docker_image          = var.paas_app_docker_image
   web_app_instances         = var.paas_web_app_instances
   web_app_memory            = var.paas_web_app_memory
-  logstash_url              = var.paas_logstash_url
+  logstash_url              = local.infra_secrets.LOGSTASH_URL
   app_environment_variables = local.paas_app_environment_variables
   docker_credentials        = local.docker_credentials
 }
 
 provider statuscake {
-  username = var.statuscake_username
-  apikey   = var.statuscake_password
+  username = local.infra_secrets.STATUSCAKE_USERNAME
+  apikey   = local.infra_secrets.STATUSCAKE_PASSWORD
 }
 
 module statuscake {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
-variable paas_user {}
-
-variable paas_password {}
-
 variable paas_sso_code {}
 
 variable paas_cf_space {}
@@ -14,15 +10,17 @@ variable paas_web_app_instances { default = 1 }
 
 variable paas_web_app_memory { default = 512 }
 
-variable paas_logstash_url {}
-
 variable paas_app_config_file { default = "./workspace_variables/app_config.yml" }
 
-variable paas_app_secrets_file { default = "./app_secrets.yml" }
+variable key_vault_name {}
 
-variable dockerhub_username {}
+variable key_vault_resource_group {}
 
-variable dockerhub_password {}
+variable key_vault_app_secret_name {}
+
+variable key_vault_infra_secret_name {}
+
+variable azure_credentials {}
 
 #StatusCake
 variable statuscake_alerts {
@@ -30,17 +28,15 @@ variable statuscake_alerts {
   default = {}
 }
 
-variable statuscake_username { default = "not-empty" }
-
-variable statuscake_password { default = "not-empty" }
-
 locals {
   cf_api_url                     = "https://api.london.cloud.service.gov.uk"
   app_config                     = yamldecode(file(var.paas_app_config_file))[var.paas_app_environment]
-  app_secrets                    = yamldecode(file(var.paas_app_secrets_file))
+  app_secrets                    = yamldecode(data.azurerm_key_vault_secret.app_secrets.value)
+  infra_secrets                  = yamldecode(data.azurerm_key_vault_secret.infra_secrets.value)
   paas_app_environment_variables = merge(local.app_config, local.app_secrets)
   docker_credentials = {
-    username = var.dockerhub_username
-    password = var.dockerhub_password
+    username = local.infra_secrets.DOCKERHUB_USERNAME
+    password = local.infra_secrets.DOCKERHUB_PASSWORD
   }
+  azure_credentials = jsondecode(var.azure_credentials)
 }
