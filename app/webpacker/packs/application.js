01679416ac913e1b786a53be5e64f707ca405cc5
Toby Retallick <toby.retallick@gmail.com>
2021-02-12 16:20:04 +0000

    Add styles and javascript for new filter toggle overlay (mobile view)

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,12 +2,14 @@ require.context("govuk-frontend/govuk/assets");
 
 import "../styles/application.scss";
 import "../images/option-select-toggle-sprite.png";
+import "../images/icon-cross.svg";
 import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
 import { initAll } from "govuk-frontend";
 import initLocationsMap from "scripts/locations-map";
 import backLink from "scripts/back-link";
 import initAutocomplete from "scripts/autocomplete";
 import toggle from "scripts/toggle";
+import { FilterToggleButton } from "scripts/filter-toggle-button"
 import { loadAnalytics } from "scripts/analytics.js";
 import initCookieBanner from "scripts/cookie-banner";
 
@@ -20,6 +22,26 @@ new backLink($backLink).init();
 
 const $toggle = document.querySelector('[data-module="toggle"]');
 new toggle($toggle).init();
+
+const filterToggleButton = new FilterToggleButton({
+  bigModeMediaQuery: '(min-width: 48.063em)',
+  startHidden: false,
+  toggleButton: {
+    container: $('.app-filter-toggle'),
+    showText: 'Show filters',
+    hideText: 'Hide filters',
+    classes: 'govuk-button--secondary'
+  },
+  closeButton: {
+    container: $('.app-filter__header'),
+    text: 'Close'
+  },
+  filter: {
+    container: $('.app-filter')
+  }
+})
+
+filterToggleButton.init()
 
 initAutocomplete({
   element: "location-autocomplete",
