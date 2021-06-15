62455c9633999ebea3deab76ed81d33fc66feb56
Misha Gorodnitzky <misaka@pobox.com>
2021-06-15 16:40:56 +0100

    [3512] add sending web request events to BigQuery

---
+++ spec/controllers/concerns/emit_request_events_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe EmitRequestEvents, type: :request, with_bigquery: true do
+  let(:provider_user) { create(:provider_user, :with_dfe_sign_in) }
+  let(:project) { instance_double(Google::Cloud::Bigquery::Project, dataset: dataset) }
+  let(:dataset) { instance_double(Google::Cloud::Bigquery::Dataset, table: table) }
+  let(:table) { instance_double(Google::Cloud::Bigquery::Table) }
+
+  before do
+    activate_feature(:send_web_requests_to_bigquery)
+    allow(Google::Cloud::Bigquery).to receive(:new).and_return(project)
+    allow(table).to receive(:insert)
+  end
+
+  it 'enqueues job to send event to bigquery' do
+    Timecop.freeze('2021-05-10 12:00:00') do
+      now = Time.zone.now
+
+      get '/', headers: {'HTTP_USER_AGENT' => 'test user agent'}
+
+      expect(SendEventToBigqueryJob).to have_been_enqueued.exactly(:once)
+
+      job_args = enqueued_jobs.last[:args]
+
+      expect(job_args.first['type']).to eq('web_request')
+      expect(job_args.first['request_path']).to eq('/')
+      expect(job_args.first['request_method']).to eq('GET')
+      expect(job_args.first['response_status']).to eq(200)
+      expect(job_args.first['user_agent']).to eq('test user agent')
+      expect(job_args.first['environment']).to eq('test')
+      expect(job_args.first['timestamp']).to eq(now.iso8601)
+    end
+  end
+end
