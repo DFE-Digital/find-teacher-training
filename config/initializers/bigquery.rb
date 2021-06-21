4764f0dcbd4005caaa3e1d7bb3c9830683e32b3d
Misha Gorodnitzky <misaka@pobox.com>
2021-06-21 16:46:32 +0100

    [3512] initialise Bigquery when feature enabled

--- config/initializers/bigquery.rb
+++ config/initializers/bigquery.rb
@@ -2,9 +2,9 @@ require 'google/cloud/bigquery'
 
 BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
 
-if BIG_QUERY_API_JSON_KEY.present?
+if FeatureFlag.active?(:send_web_requests_to_bigquery)
   Google::Cloud::Bigquery.configure do |config|
-    config.project_id  = ENV.fetch('BIG_QUERY_PROJECT_ID')
+    config.project_id  = Settings.google.big_query_project_id
     config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
   end
 
