a69d8049102d9b57b901f8a904be20fa5de1393c
Misha Gorodnitzky <misaka@pobox.com>
2021-06-28 18:08:29 +0100

    [3512] tidy up BigQuery settings and init
    - Create Settings.google.big_query settings hash
    - Make it clear that the initialiser doesn't actually validate anything
      but the format of the JSON API key supplied.

--- config/initializers/big_query.rb
+++ config/initializers/big_query.rb
@@ -3,8 +3,11 @@ require 'google/cloud/bigquery'
 BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
 
 if FeatureFlag.active?(:send_web_requests_to_big_query)
+  # Validate that the JSON key exists and that it is parseable.
+  raise 'BigQuery JSON key missing. Disable feature if not sending events to BigQuery.' if BIG_QUERY_API_JSON_KEY.blank?
+
   Google::Cloud::Bigquery.configure do |config|
-    config.project_id  = Settings.google.bigquery_project_id
+    # This command doesn't actually connect to BigQuery to check authorisation.
     config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
   end
 
