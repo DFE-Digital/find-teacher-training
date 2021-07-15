eb47e29c0d2cdd0076cc761e1463711a2974d588
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-07-15 14:14:27 +0100

    Rename to allowed_parameters
    canonical_rails has renamed to allowed_parameters

--- config/initializers/canonical_rails.rb
+++ config/initializers/canonical_rails.rb
@@ -16,14 +16,14 @@ CanonicalRails.setup do |config|
   # Trailing slash represents semantics of a directory, ie a collection view - implying an :index get route;
   # otherwise we have to assume semantics of an instance of a resource type, a member view - implying a :show get route
   #
-  # Acts as a whitelist for routes to have trailing slashes
+  # Acts as a allowlist for routes to have trailing slashes
 
   config.collection_actions # = [:index]
 
   # Parameter spamming can cause index dilution by creating seemingly different URLs with identical or near-identical content.
-  # Unless whitelisted, these parameters will be omitted
+  # Unless allowed, these parameters will be omitted
 
-  config.whitelisted_parameters # = []
+  config.allowed_parameters # = []
 
   # Output a matching OpenGraph URL meta tag (og:url) with the canonical URL, as recommended by Facebook et al
   config.opengraph_url = true
