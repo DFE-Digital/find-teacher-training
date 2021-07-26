3bc2e2a4df6400cbda3d2c043d8955385d04197a
Steve Hook <steve.hook@gmail.com>
2021-07-26 22:28:31 +0100

    InternationalStudentsComponent#visa_sponsorship_status depends on course
    The Course#funding_type determines which kind of visa is approriate.

--- app/components/courses/international_students_component.rb
+++ app/components/courses/international_students_component.rb
@@ -4,16 +4,18 @@ module Courses
 
     TRAIN_TO_TEACH_URL = 'https://www.gov.uk/government/publications/train-to-teach-in-england-non-uk-applicants/train-to-teach-in-england-non-uk-applicants#visas-and-immigration'.freeze
 
-    def initialize(provider:)
-      @provider = provider
+    def initialize(course:)
+      @course = course
+    end
+
+    def provider
+      @provider ||= @course.provider
     end
 
     def visa_sponsorship_status
-      if @provider.can_sponsor_all_visas?
-        "We can sponsor #{govuk_link_to('Student and Skilled Worker visas', TRAIN_TO_TEACH_URL)}, but this is not guaranteed.".html_safe
-      elsif @provider.can_only_sponsor_student_visa?
+      if !@course.salaried? && provider.can_sponsor_student_visa
         "We can sponsor #{govuk_link_to('Student visas', TRAIN_TO_TEACH_URL)}, but this is not guaranteed.".html_safe
-      elsif @provider.can_only_sponsor_skilled_worker_visa?
+      elsif @course.salaried? && provider.can_sponsor_skilled_worker_visa
         "We can sponsor #{govuk_link_to('Skilled Worker visas', TRAIN_TO_TEACH_URL)}, but this is not guaranteed.".html_safe
       else
         "We cannot sponsor visas. You will need to #{govuk_link_to('get the right visa or status to study in the UK', TRAIN_TO_TEACH_URL)}.".html_safe
--- spec/componenets/courses/international_students_component_spec.rb
+++ spec/componenets/courses/international_students_component_spec.rb
@@ -8,37 +8,52 @@ describe Courses::InternationalStudentsComponent do
         can_sponsor_student_visa: false,
         can_sponsor_skilled_worker_visa: false,
       )
-      result = render_inline(described_class.new(provider: provider))
+      course = build(
+        :course,
+        funding_type: 'fee',
+        provider: provider,
+      )
+      result = render_inline(described_class.new(course: CourseDecorator.new(course)))
 
       expect(result.text).to include('We cannot sponsor visas. You will need to get the right visa or status to study in the UK')
       expect(result).to have_selector("a[href='#{described_class::TRAIN_TO_TEACH_URL}']")
     end
   end
 
-  context 'when the provider sponsors student visa', type: :component do
+  context 'when the provider sponsors student visa the course is not salaried', type: :component do
     it 'renders correct message' do
       provider = build(
         :provider,
         can_sponsor_student_visa: true,
         can_sponsor_skilled_worker_visa: false,
       )
-      result = render_inline(described_class.new(provider: provider))
+      course = build(
+        :course,
+        funding_type: 'fee',
+        provider: provider,
+      )
+      result = render_inline(described_class.new(course: CourseDecorator.new(course)))
 
       expect(result.text).to include('We can sponsor Student visas, but this is not guaranteed.')
       expect(result).to have_selector("a[href='#{described_class::TRAIN_TO_TEACH_URL}']")
     end
   end
 
-  context 'when the provider sponsors both kinds of visa', type: :component do
+  context 'when the provider sponsors both kinds of visa and the course is salaried', type: :component do
     it 'renders correct message' do
       provider = build(
         :provider,
         can_sponsor_student_visa: true,
         can_sponsor_skilled_worker_visa: true,
       )
-      result = render_inline(described_class.new(provider: provider))
+      course = build(
+        :course,
+        funding_type: 'salary',
+        provider: provider,
+      )
+      result = render_inline(described_class.new(course: CourseDecorator.new(course)))
 
-      expect(result.text).to include('We can sponsor Student and Skilled Worker visas, but this is not guaranteed.')
+      expect(result.text).to include('We can sponsor Skilled Worker visas, but this is not guaranteed.')
       expect(result).to have_selector("a[href='#{described_class::TRAIN_TO_TEACH_URL}']")
     end
   end
