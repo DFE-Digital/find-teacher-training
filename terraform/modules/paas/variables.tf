249ec7585d3a44056de7eee98e46212b8fe6d41b
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-09-15 17:28:16 +0100

    Terraform config for binding assets hostname

--- terraform/modules/paas/variables.tf
+++ terraform/modules/paas/variables.tf
@@ -47,6 +47,15 @@ locals {
     prod    = ["www", "www2"]
     review  = [local.app_name_suffix]
   }
+  assets_host_names = {
+    qa        = "qa-assets"
+    staging   = "staging-assets"
+    sandbox   = "sandbox-assets"
+    load-test = "assets"
+    prod      = "assets"
+    review    = "${local.app_name_suffix}-assets"
+  }
   web_app_service_gov_uk_route_ids = [for r in cloudfoundry_route.web_app_service_gov_uk_route : r.id]
-  web_app_routes                   = concat(local.web_app_service_gov_uk_route_ids, [cloudfoundry_route.web_app_cloudapps_digital_route.id])
+  web_app_routes = concat(local.web_app_service_gov_uk_route_ids,
+  [cloudfoundry_route.web_app_cloudapps_digital_route.id, cloudfoundry_route.web_app_assets_service_gov_uk_route.id])
 }
