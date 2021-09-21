5b3ac568f63b593ede8d50daffcd4f00ec617580
davidgisbey <davidgisbey@gmail.com>
2021-09-21 13:02:02 +0100

    Add feature spec

---
+++ spec/features/maintainance_page_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'View pages', type: :feature do
+  context 'given the maintainance_mode feature flag is active and i arrive at the site' do
+    it 'sends me to the maintainance page' do
+      activate_feature(:maintainance_mode)
+
+      visit '/'
+
+      expect(page).to have_current_path maintainance_path
+    end
+  end
+
+  context 'given the maintainance_mode feature flag is deactive and i visit the maintainance_path' do
+    it 'sends me to the homepage' do
+      visit maintainance_path
+
+      expect(page).to have_current_path root_path
+    end
+  end
+end
