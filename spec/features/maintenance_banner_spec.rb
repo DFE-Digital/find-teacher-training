2bb1e2f60ff06d7997f98f8f6108ea2eb874ac14
davidgisbey <davidgisbey@gmail.com>
2021-09-21 15:44:51 +0100

    Add feature spec for the banner rendering

---
+++ spec/features/maintenance_banner_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Maintenance banner', type: :feature do
+  context 'given the maintenance_mode feature flag is active and i arrive at the site' do
+    it 'sends me to the maintenance page' do
+      activate_feature(:maintenance_banner)
+
+      visit '/'
+
+      expect(page).to have_content 'This service will be unavailable on'
+    end
+  end
+
+  context 'given the maintenance_banner feature flag is deactive and i visit the homepage' do
+    it 'sends me to the homepage' do
+      visit '/'
+
+      expect(page).not_to have_content 'This service will be unavailable on'
+    end
+  end
+end
