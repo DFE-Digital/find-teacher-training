701b3560bb63554ad170bd3edcab29d93b09e9d1
davidgisbey <davidgisbey@gmail.com>
2021-09-22 16:29:48 +0100

    Do not show maintenance banner on maintenance page

--- app/components/maintenance_banner_component.rb
+++ app/components/maintenance_banner_component.rb
@@ -1,6 +1,6 @@
 class MaintenanceBannerComponent < ViewComponent::Base
   include ViewHelper
   def render?
-    FeatureFlag.active?(:maintenance_banner)
+    FeatureFlag.active?(:maintenance_banner) && !FeatureFlag.active?(:maintenance_mode)
   end
 end
