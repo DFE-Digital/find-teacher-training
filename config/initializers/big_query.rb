7bafc8553218f57fb6e56fb83315a88c484b8fe6
Duncan Brown <duncanjbrown@gmail.com>
2021-09-28 20:56:44 +0100

    Fix deprecation warning about autoloading constants at init

--- config/initializers/big_query.rb
+++ config/initializers/big_query.rb
@@ -2,7 +2,13 @@ require 'google/cloud/bigquery'
 
 BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
 
-if FeatureFlag.active?(:send_web_requests_to_big_query)
+# https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoloading-when-the-application-boots
+big_query_enabled = false
+Rails.application.reloader.to_prepare do
+  big_query_enabled = FeatureFlag.active?(:send_web_requests_to_big_query)
+end
+
+if big_query_enabled
   # Validate that the JSON key exists and that it is parseable.
   raise 'BigQuery JSON key missing. Disable feature if not sending events to BigQuery.' if BIG_QUERY_API_JSON_KEY.blank?
 
