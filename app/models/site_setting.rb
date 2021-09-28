3524de8750ab5ff2d5157837ed1df2b35317edff
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-09-28 14:45:26 +0100

    Use Redis.current for SiteSetting

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -2,10 +2,10 @@ require './app/lib/redis'
 
 class SiteSetting < Base
   def self.cycle_schedule
-    RedisService.new.get('cycle_schedule')&.to_sym || :real
+    RedisService.current.get('cycle_schedule')&.to_sym || :real
   end
 
   def self.set(name:, value:)
-    RedisService.new.set(name, value)
+    RedisService.current.set(name, value)
   end
 end
