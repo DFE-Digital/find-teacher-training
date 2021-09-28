dd5f1803d0bfeff4c014882b67bc0ac70b833ac9
Malcolm Baig <git@baig.org>
2021-09-28 11:05:25 +0100

    Cache subject API calls
    Subjects and subject areas don't change very often. Cache their API
    calls for an hour.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,12 @@
 class Subject < Base
+  TTAPI_CALLS_EXPIRY = 1.hour
+
+  connection do |conn|
+    conn.faraday.response(:caching, write_options: { expires_in: TTAPI_CALLS_EXPIRY }) do
+      Rails.cache
+    end
+  end
+
   has_many :course_subjects
   has_many :courses, through: :course_subjects
   belongs_to :subject_area, foreign_key: :type, inverse_of: :subjects, shallow_path: true
--- app/models/subject_area.rb
+++ app/models/subject_area.rb
@@ -1,4 +1,12 @@
 class SubjectArea < Base
+  TTAPI_CALLS_EXPIRY = 1.hour
+
+  connection do |conn|
+    conn.faraday.response(:caching, write_options: { expires_in: TTAPI_CALLS_EXPIRY }) do
+      Rails.cache
+    end
+  end
+
   has_many :subjects, foreign_key: :type, inverse_of: :subject_area
   self.primary_key = :typename
 end
