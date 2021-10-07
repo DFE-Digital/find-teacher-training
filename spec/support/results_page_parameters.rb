0687ecc1aa57ff6b5848942246d171222b372fa3
Toby Retallick <toby.retallick@gmail.com>
2021-10-07 17:32:58 +0100

    Use sparse fields when calling the Teacher Training API
    - We want minimise the size of the payload from the Teacher Training API by only selecting the fields we require

--- spec/support/results_page_parameters.rb
+++ spec/support/results_page_parameters.rb
@@ -1,7 +1,13 @@
 def results_page_parameters(parameters = {})
   {
     'filter[has_vacancies]' => 'true',
-    'include' => 'site_statuses.site,provider,subjects',
+    'include' => 'site_statuses.site,provider',
+    'fields' =>
+      {
+        'courses' => 'name,course_code,provider_code,study_mode,qualification,funding_type,provider_type,level,provider,site_statuses',
+        'providers' => 'provider_name,address1,address2,address3,address4,postcode',
+        'site_statuses' => 'status,has_vacancies?,site',
+      },
     'page[page]' => 1,
     'page[per_page]' => 30,
     'sort' => 'provider.provider_name,name',
