581b847b81077de77233d3468501a55cbcffc1e0
Duncan Brown <duncanjbrown@gmail.com>
2021-10-08 12:19:30 +0100

    Reject invalid multipart form data using Rack middleware
    Our Csharp subject param converting middleware needs to look at the
    request params, but if these are malformed Rack throws an exception.
    Handle this exception - which is the client's fault - in outer middleware.
    
    Example sentry error:
    https://sentry.io/organizations/dfe-bat/issues/2685501784/?referrer=slack

---
+++ app/middleware/handle_bad_multipart_form_data_middleware.rb
@@ -0,0 +1,17 @@
+class HandleBadMultipartFormDataMiddleware
+  # Rack panics when calling params on req with invalid multipart form data
+  # but we need to look at params as part of the Csharp param handling middleware
+  # https://github.com/rack/rack/issues/903
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    begin
+      @app.call(env)
+    rescue EOFError
+      [400, { 'Content-Type' => 'text/plain' }, ['Bad Request']]
+    end
+  end
+end
---
+++ spec/requests/bad_form_data_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'handling invalid multipart form data' do
+  it 'returns 400 for malformed form data' do
+    # POST to /cookies (a valid POST path) so we don't get a 404 exception instead
+    post '/cookies', params: 'nonsense', headers: { 'Content-Type' => 'multipart/form-data; boundary=----blah' }
+    expect(response).to have_http_status(400)
+  end
+end
