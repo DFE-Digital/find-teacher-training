81a7e78512325bb19b7cde140a861e5d8a089124
Toby Retallick <toby.retallick@gmail.com>
2021-10-11 15:24:07 +0100

    Only render cache providers autocomplete if there are providers in the cache

--- app/helpers/select_options_helper.rb
+++ app/helpers/select_options_helper.rb
@@ -2,6 +2,8 @@ module SelectOptionsHelper
   def select_provider_options
     provider_options = JSON.parse(TeacherTrainingPublicAPI::ProvidersCache.read)
 
-    [OpenStruct.new(id: '', name: 'Select a provider')] + provider_options.map { |provider| OpenStruct.new(id: "#{provider['name']} (#{provider['code']})", name: "#{provider['name']} (#{provider['code']})") }
+    if provider_options
+      [OpenStruct.new(id: '', name: 'Select a provider')] + provider_options.map { |provider| OpenStruct.new(id: "#{provider['name']} (#{provider['code']})", name: "#{provider['name']} (#{provider['code']})") }
+    end
   end
 end
