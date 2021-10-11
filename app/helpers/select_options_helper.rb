015047fbb6501fec68ea4ca8dab99878245d8291
Toby Retallick <toby.retallick@gmail.com>
2021-10-11 16:15:29 +0100

    Add test coverage for rendering cached providers autocomplete

--- app/helpers/select_options_helper.rb
+++ app/helpers/select_options_helper.rb
@@ -1,9 +1,8 @@
 module SelectOptionsHelper
   def select_provider_options
-    provider_options = JSON.parse(TeacherTrainingPublicAPI::ProvidersCache.read)
+    cached_providers = TeacherTrainingPublicAPI::ProvidersCache.read
+    return if cached_providers.blank?
 
-    if provider_options
-      [OpenStruct.new(id: '', name: 'Select a provider')] + provider_options.map { |provider| OpenStruct.new(id: "#{provider['name']} (#{provider['code']})", name: "#{provider['name']} (#{provider['code']})") }
-    end
+    [OpenStruct.new(id: '', name: 'Select a provider')] + JSON.parse(cached_providers).map { |provider| OpenStruct.new(id: "#{provider['name']} (#{provider['code']})", name: "#{provider['name']} (#{provider['code']})") }
   end
 end
--- spec/requests/start_spec.rb
+++ spec/requests/start_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe '/start', type: :request do
+  let(:provider_autocomplete_container_div) { '<div id="provider-autocomplete" class="govuk-body"></div>' }
+  let(:cached_providers_data_qa_attr) { 'cached-providers-autocomplete' }
+
   it "redirects from '/start/location' to '/'" do
     get '/start/location'
 
@@ -8,20 +11,59 @@ describe '/start', type: :request do
   end
 
   describe 'provider autocomplete' do
-    let(:container_div) { '<div id="provider-autocomplete" class="govuk-body"></div>' }
-
     it 'is present when provider_autocomplete feature flag is active' do
       activate_feature(:provider_autocomplete)
+
       get '/'
 
-      expect(response.body).to include(container_div)
+      expect(response.body).to include(provider_autocomplete_container_div)
+      expect(response.body).not_to include(cached_providers_data_qa_attr)
     end
 
     it 'is not present when provider_autocomplete feature flag is inactive' do
       deactivate_feature(:provider_autocomplete)
       get '/'
 
-      expect(response.body).not_to include(container_div)
+      expect(response.body).not_to include(provider_autocomplete_container_div)
+      expect(response.body).not_to include(cached_providers_data_qa_attr)
+    end
+  end
+
+  describe 'cached provider autocomplete' do
+    let(:cached_providers) {
+      [
+        {
+          name: 'Provider 1',
+          code: 'ABC',
+        },
+      ].to_json
+    }
+
+    it 'is present when cached provider feature flag is active and there are providers stored in the cache' do
+      allow(TeacherTrainingPublicAPI::ProvidersCache).to receive(:read).and_return(cached_providers)
+      activate_feature(:cache_providers)
+
+      get '/'
+
+      expect(response.body).to include(cached_providers_data_qa_attr)
+    end
+
+    it 'is not present when provider_autocomplete feature flag is inactive' do
+      deactivate_feature(:cached_providers)
+      allow(TeacherTrainingPublicAPI::ProvidersCache).to receive(:read).and_return(cached_providers)
+
+      get '/'
+
+      expect(response.body).not_to include(cached_providers_data_qa_attr)
+    end
+
+    it 'is not present when there are no cached providers' do
+      activate_feature(:cache_providers)
+      allow(TeacherTrainingPublicAPI::ProvidersCache).to receive(:read).and_return(nil)
+
+      get '/'
+
+      expect(response.body).not_to include(cached_providers_data_qa_attr)
     end
   end
 end
