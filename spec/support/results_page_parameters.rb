c3eb2ba80a917ff86f02d4540c11697271bca9dd
Toby Retallick <toby.retallick@gmail.com>
2021-10-12 10:27:15 +0100

    Add 'scholarship' and 'bursary_amount' to sparse fields

--- spec/support/results_page_parameters.rb
+++ spec/support/results_page_parameters.rb
@@ -7,7 +7,7 @@ def results_page_parameters(parameters = {})
         'courses' => 'name,course_code,provider_code,study_mode,qualification,funding_type,provider_type,level,provider,site_statuses,subjects',
         'providers' => 'provider_name,address1,address2,address3,address4,postcode',
         'site_statuses' => 'status,has_vacancies?,site',
-        'subjects' => 'subject_name,subject_code',
+        'subjects' => 'subject_name,subject_code,bursary_amount,scholarship',
       },
     'page[page]' => 1,
     'page[per_page]' => 30,
