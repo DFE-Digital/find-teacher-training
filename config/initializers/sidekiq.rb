2936dffe03bf22c140a702da497cee7eb2ea3293
Toby Retallick <toby.retallick@gmail.com>
2021-10-19 15:43:20 +0100

    Fix Sidekiq cron job
    - Add missing queue to skidekik.yml
    - Load sidekiq cron jobs after application initialisation (else the cron
      job will not register)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,14 +5,16 @@ Sidekiq.configure_server do |config|
     url: RedisService.redis_url,
   }
   config.logger.level = Logger::WARN
-
-  if Settings.background_jobs
-    Sidekiq::Cron::Job.load_from_hash Settings.background_jobs
-  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
     url: RedisService.redis_url,
   }
+end
+
+Rails.application.config.after_initialize do
+  if Settings.background_jobs
+    Sidekiq::Cron::Job.load_from_hash Settings.background_jobs
+  end
 end
