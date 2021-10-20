ecf70339eef0a290ff46d6ca19b21269bc36c32a
Toby Retallick <toby.retallick@gmail.com>
2021-10-20 15:37:43 +0100

    Render cached providers autocomplete and fall back to non-js provider search
    - The old provider autocomplete will no longer render

--- spec/requests/start_spec.rb
+++ spec/requests/start_spec.rb
@@ -1,32 +1,12 @@
 require 'rails_helper'
 
 describe '/start', type: :request do
-  let(:provider_autocomplete_container_div) { '<div id="provider-autocomplete" class="govuk-body"></div>' }
   let(:cached_providers_data_qa_attr) { 'cached-providers-autocomplete' }
 
   it "redirects from '/start/location' to '/'" do
     get '/start/location'
 
     expect(response).to redirect_to('/')
-  end
-
-  describe 'provider autocomplete' do
-    it 'is present when provider_autocomplete feature flag is active' do
-      activate_feature(:provider_autocomplete)
-
-      get '/'
-
-      expect(response.body).to include(provider_autocomplete_container_div)
-      expect(response.body).not_to include(cached_providers_data_qa_attr)
-    end
-
-    it 'is not present when provider_autocomplete feature flag is inactive' do
-      deactivate_feature(:provider_autocomplete)
-      get '/'
-
-      expect(response.body).not_to include(provider_autocomplete_container_div)
-      expect(response.body).not_to include(cached_providers_data_qa_attr)
-    end
   end
 
   describe 'cached provider autocomplete' do
@@ -39,26 +19,15 @@ describe '/start', type: :request do
       ].to_json
     }
 
-    it 'is present when cached provider feature flag is active and there are providers stored in the cache' do
+    it 'is present when there are providers stored in the cache' do
       allow(TeacherTrainingPublicAPI::ProvidersCache).to receive(:read).and_return(cached_providers)
-      activate_feature(:cache_providers)
 
       get '/'
 
       expect(response.body).to include(cached_providers_data_qa_attr)
     end
 
-    it 'is not present when provider_autocomplete feature flag is inactive' do
-      deactivate_feature(:cached_providers)
-      allow(TeacherTrainingPublicAPI::ProvidersCache).to receive(:read).and_return(cached_providers)
-
-      get '/'
-
-      expect(response.body).not_to include(cached_providers_data_qa_attr)
-    end
-
     it 'is not present when there are no cached providers' do
-      activate_feature(:cache_providers)
       allow(TeacherTrainingPublicAPI::ProvidersCache).to receive(:read).and_return(nil)
 
       get '/'
