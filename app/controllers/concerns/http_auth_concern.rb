ecb1aba8b6ce4e82ac2c013dcd1162b53ed64e29
Toby Retallick <toby.retallick@gmail.com>
2021-11-08 20:15:12 +0000

    Create basic auth controller concern

---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,15 @@
+module HttpAuthConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :http_authenticate
+  end
+
+  def http_authenticate
+    return true unless Settings.basic_auth_enabled
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Settings.basic_auth_username && password == Settings.basic_auth_password
+    end
+  end
+end
