9c5c820236f84464f9e04cb2d2f53621a0c95448
Toby Retallick <toby.retallick@gmail.com>
2021-11-24 13:50:53 +0000

    Extract Courses::SummaryComponent

---
+++ spec/components/courses/summary_component_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe Courses::SummaryComponent, type: :component do
+  it 'renders sub sections' do
+    provider = build(:provider).decorate
+    course = build(:course,
+                   provider: provider,
+                   course_length: '1 year').decorate
+
+    result = render_inline(described_class.new(course))
+
+    expect(result.text).to include(
+      'Financial support',
+      'Qualification',
+      'Course length',
+      'Qualification',
+      'Date you can apply from',
+      'Date course starts',
+      'Website',
+    )
+  end
+
+  context 'a course has an accrediting provider that is not the provider' do
+    it 'renders the accredited body' do
+      course = build(
+        :course,
+        provider: build(:provider),
+        accrediting_provider: build(:provider),
+      ).decorate
+
+      result = render_inline(described_class.new(course))
+
+      expect(result.text).to include(
+        'Accredited body',
+      )
+    end
+  end
+
+  context 'the course provider and accrediting provider are the same' do
+    it 'does not render the accredited body' do
+      provider = build(:provider)
+
+      course = build(
+        :course,
+        provider: provider,
+        accrediting_provider: provider,
+      ).decorate
+
+      result = render_inline(described_class.new(course))
+
+      expect(result.text).not_to include(
+        'Accredited body',
+      )
+    end
+  end
+end
