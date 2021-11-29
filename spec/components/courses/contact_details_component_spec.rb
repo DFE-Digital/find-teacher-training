4b103ad45991e4e54429ded3589a97b843d1a3ff
Toby Retallick <toby.retallick@gmail.com>
2021-11-29 11:05:41 +0000

    Extract Courses::ContactDetailsComponent
    - Remove duplicate specs
    - Remove legacy partial

---
+++ spec/components/courses/contact_details_component_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Courses::ContactDetailsComponent, type: :component do
+  context 'contact details for London School of Jewish Studies and the course code is X104' do
+    it 'renders the custom address requested via zendesk' do
+      provider = build(:provider, provider_code: '28T')
+      course = build(:course, course_code: 'X104', provider: provider).decorate
+
+      result = render_inline(described_class.new(course))
+
+      expect(result.text).to include('LSJS','44A Albert Road','London','NW4 2SJ')
+    end
+  end
+end
