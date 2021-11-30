0d12bd4ec9b0e433bc1b4387593aab86f72b97e0
Toby Retallick <toby.retallick@gmail.com>
2021-11-30 14:44:16 +0000

    Refactor spec to conform to BDD style

--- spec/features/result_filters/salary_spec.rb
+++ spec/features/result_filters/salary_spec.rb
@@ -3,58 +3,50 @@ require 'rails_helper'
 RSpec.feature 'Results page new funding filter' do
   include StubbedRequests::Courses
   include StubbedRequests::Subjects
+  include FiltersFeatureSpecsHelper
 
-  let(:results_page) { PageObjects::Page::Results.new }
-  let(:base_parameters) { results_page_parameters }
+  scenario 'Candidate applies salary filter' do
+    when_i_visit_the_results_page
+    then_i_see_that_the_salary_checkbox_is_not_selected
 
-  before do
-    stub_subjects
-    stub_courses(query: base_parameters, course_count: 10)
+    when_i_select_the_salary_checkbox
+    and_apply_the_filters
+    then_i_see_that_the_salary_checkbox_is_selected
+    and_the_salary_query_parameter_is_retained
   end
 
-  describe 'viewing results without explicitly de-selecting a filter' do
-    it 'shows courses with or without a salary' do
-      results_page.load
-
-      expect(results_page.funding_filter.legend.text).to eq('Salary')
-      expect(results_page.funding_filter.checkbox.checked?).to be(false)
-    end
+  def then_i_see_that_the_salary_checkbox_is_not_selected
+    expect(results_page.funding_filter.legend.text).to eq('Salary')
+    expect(results_page.funding_filter.checkbox.checked?).to be(false)
   end
 
-  describe 'applying the filters' do
-    context 'shows only courses with a salary only' do
-      before do
-        stub_courses(
-          query: base_parameters.merge(
-            'filter[funding]' => 'salary',
-            'filter[study_type]' => 'full_time,part_time',
-          ),
-          course_count: 10,
-        )
+  def when_i_select_the_salary_checkbox
+    stub_courses(
+      query: results_page_parameters.merge(
+        'filter[funding]' => 'salary',
+        'filter[study_type]' => 'full_time,part_time',
+      ),
+      course_count: 10,
+    )
 
-        results_page.load
-        results_page.funding_filter.checkbox.check
-        results_page.apply_filters_button.click
-      end
+    results_page.funding_filter.checkbox.check
+  end
 
-      it 'list the filtered courses' do
-        expect(results_page.funding_filter.legend.text).to eq('Salary')
-        expect(results_page.funding_filter.checkbox.checked?).to be(true)
-      end
+  def then_i_see_that_the_salary_checkbox_is_selected
+    expect(results_page.funding_filter.checkbox.checked?).to be(true)
+  end
 
-      it 'retains the query parameters' do
-        expect_page_to_be_displayed_with_query(
-          page: results_page,
-          expected_query_params: {
-            'fulltime' => 'true',
-            'parttime' => 'true',
-            'hasvacancies' => 'true',
-            'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
-            'degree_required' => 'show_all_courses',
-            'funding' => '8',
-          },
-        )
-      end
-    end
+  def and_the_salary_query_parameter_is_retained
+    expect_page_to_be_displayed_with_query(
+      page: results_page,
+      expected_query_params: {
+        'fulltime' => 'true',
+        'parttime' => 'true',
+        'hasvacancies' => 'true',
+        'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
+        'degree_required' => 'show_all_courses',
+        'funding' => '8',
+      },
+    )
   end
 end
