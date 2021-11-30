ff31b72b6fe0cee78cad0c279be303c6e66e7c23
Toby Retallick <toby.retallick@gmail.com>
2021-11-30 15:00:09 +0000

    Refactor spec to conform to BDD style

--- spec/features/result_filters/send_spec.rb
+++ spec/features/result_filters/send_spec.rb
@@ -3,66 +3,50 @@ require 'rails_helper'
 RSpec.feature 'Results page new SEND filter' do
   include StubbedRequests::Courses
   include StubbedRequests::Subjects
+  include FiltersFeatureSpecsHelper
 
-  let(:results_page) { PageObjects::Page::Results.new }
-  let(:base_parameters) { results_page_parameters }
+  scenario 'Candidate applies the SEND filter' do
+    when_i_visit_the_results_page
+    then_i_see_that_the_send_checkbox_is_not_selected
 
-  before do
-    stub_subjects
-    stub_courses(query: base_parameters, course_count: 10)
+    when_i_select_the_send_checkbox
+    and_apply_the_filters
+    then_i_see_that_the_send_checkbox_is_selected
+    and_the_send_query_parameter_is_retained
   end
 
-  describe 'viewing results without explicitly selecting a filter' do
-    it 'show courses with or without a SEND specialism' do
-      results_page.load
-
-      expect(results_page.send_filter.legend.text).to eq('Special educational needs')
-      expect(results_page.send_filter.checkbox.checked?).to be(false)
-    end
+  def then_i_see_that_the_send_checkbox_is_not_selected
+    expect(results_page.send_filter.legend.text).to eq('Special educational needs')
+    expect(results_page.send_filter.checkbox.checked?).to be(false)
   end
 
-  describe 'applying the filter' do
-    before do
-      stub_courses(
-        query: base_parameters.merge(
-          'filter[subjects]' => '00',
-        ),
-        course_count: 10,
-      )
-      stub_courses(
-        query: base_parameters.merge(
-          'filter[send_courses]' => 'true',
-          'filter[study_type]' => 'full_time,part_time',
-          'filter[subjects]' => '00',
-        ),
-        course_count: 10,
-      )
+  def when_i_select_the_send_checkbox
+    stub_courses(
+      query: results_page_parameters.merge(
+        'filter[send_courses]' => 'true',
+        'filter[study_type]' => 'full_time,part_time',
+      ),
+      course_count: 10,
+    )
 
-      visit results_path(subject_codes: ['00'])
-      results_page.send_filter.checkbox.check
-      results_page.apply_filters_button.click
-    end
+    results_page.send_filter.checkbox.check
+  end
 
-    context 'selecting courses with a SEND specialism' do
-      it 'list the filtered courses' do
-        expect(results_page.send_filter.legend.text).to eq('Special educational needs')
-        expect(results_page.send_filter.checkbox.checked?).to be(true)
-      end
+  def then_i_see_that_the_send_checkbox_is_selected
+    expect(results_page.send_filter.checkbox.checked?).to be(true)
+  end
 
-      it 'retains the query parameters' do
-        expect_page_to_be_displayed_with_query(
-          page: results_page,
-          expected_query_params: {
-            'fulltime' => 'true',
-            'parttime' => 'true',
-            'hasvacancies' => 'true',
-            'degree_required' => 'show_all_courses',
-            'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
-            'senCourses' => 'true',
-            'subject_codes' => ['00'],
-          },
-        )
-      end
-    end
+  def and_the_send_query_parameter_is_retained
+    expect_page_to_be_displayed_with_query(
+      page: results_page,
+      expected_query_params: {
+        'fulltime' => 'true',
+        'parttime' => 'true',
+        'hasvacancies' => 'true',
+        'degree_required' => 'show_all_courses',
+        'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
+        'senCourses' => 'true',
+      },
+    )
   end
 end
