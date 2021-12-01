2f4514de90782b8a73ebc5d2782fe8b1928a7a09
Toby Retallick <toby.retallick@gmail.com>
2021-12-01 12:01:48 +0000

    Remove unused SitePrism object

--- spec/site_prism/page_objects/page/result_page_filters/location_filter_results.rb
+++
@@ -1,9 +0,0 @@
-module PageObjects
-  module Page
-    module ResultPageFilters
-      class LocationFilterResults < SitePrism::Page
-        set_url '/results?l=1&lat=51.4980188&lng=-0.1300436&loc=Westminster%2C+London+SW1P+3BT%2C+UK&lq=SW1P+3BT&rad=5'
-      end
-    end
-  end
-end
