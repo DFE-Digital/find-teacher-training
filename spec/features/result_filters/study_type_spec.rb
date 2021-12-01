0163c0ccdac967a2246b3156494b61eb7d87e624
Toby Retallick <toby.retallick@gmail.com>
2021-12-01 10:33:25 +0000

    Refactor spec to conform to BDD style

--- spec/features/result_filters/study_type_spec.rb
+++ spec/features/result_filters/study_type_spec.rb
@@ -1,125 +1,97 @@
 require 'rails_helper'
 
-RSpec.feature 'Results page new study type filter' do
+RSpec.feature 'Study type filter' do
   include StubbedRequests::Courses
   include StubbedRequests::Subjects
+  include FiltersFeatureSpecsHelper
 
-  let(:results_page) { PageObjects::Page::Results.new }
-  let(:base_parameters) { results_page_parameters }
+  scenario 'Candidate applies study type filters on results page' do
+    when_i_visit_the_results_page
+    then_i_see_both_study_type_checkboxes_are_selected
 
-  before do
-    stub_subjects
-    stub_courses(query: base_parameters, course_count: 10)
+    when_i_unselect_the_part_time_study_checkbox
+    and_apply_the_filters
+    then_i_see_that_the_full_time_study_checkbox_is_still_selected
+    and_the_part_time_study_checkbox_is_unselected
+    and_the_full_time_study_query_parameters_are_retained
+
+    when_i_unselect_the_full_time_study_checkbox
+    and_i_select_the_part_time_study_checkbox
+    and_apply_the_filters
+    then_i_see_that_the_part_time_study_checkbox_is_still_selected
+    and_the_full_time_study_checkbox_is_unselected
+    and_the_part_time_study_query_parameters_are_retained
   end
 
-  describe 'viewing results without explicitly de-selecting a filter' do
-    it 'show courses with all study types selected' do
-      results_page.load
-
-      expect(results_page.study_type_filter.legend.text).to eq('Study type')
-      expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(true)
-      expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(true)
-    end
+  def then_i_see_both_study_type_checkboxes_are_selected
+    expect(results_page.study_type_filter.legend.text).to eq('Study type')
+    expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(true)
+    expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(true)
   end
 
-  describe 'applying the filters' do
-    context 'show full time courses only' do
-      before do
-        stub_courses(
-          query: base_parameters.merge(
-            'filter[study_type]' => 'full_time',
-          ),
-          course_count: 10,
-        )
+  def when_i_unselect_the_part_time_study_checkbox
+    stub_courses(
+      query: results_page_parameters.merge(
+        'filter[study_type]' => 'full_time',
+      ),
+      course_count: 10,
+    )
 
-        results_page.load
-        results_page.study_type_filter.parttime_checkbox.uncheck
-        results_page.apply_filters_button.click
-      end
+    results_page.study_type_filter.parttime_checkbox.uncheck
+  end
 
-      it 'list the filtered courses' do
-        expect(results_page.study_type_filter.legend.text).to eq('Study type')
-        expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(false)
-        expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(true)
-      end
+  def then_i_see_that_the_full_time_study_checkbox_is_still_selected
+    expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(true)
+  end
 
-      it 'retains the query parameters' do
-        expect_page_to_be_displayed_with_query(
-          page: results_page,
-          expected_query_params: {
-            'fulltime' => 'true',
-            'hasvacancies' => 'true',
-            'degree_required' => 'show_all_courses',
-            'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
-          },
-        )
-      end
-    end
+  def and_the_part_time_study_checkbox_is_unselected
+    expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(false)
+  end
 
-    context 'show part time courses only' do
-      before do
-        stub_courses(
-          query: base_parameters.merge(
-            'filter[study_type]' => 'part_time',
-          ),
-          course_count: 10,
-        )
+  def and_the_full_time_study_query_parameters_are_retained
+    expect_page_to_be_displayed_with_query(
+      page: results_page,
+      expected_query_params: {
+        'fulltime' => 'true',
+        'hasvacancies' => 'true',
+        'degree_required' => 'show_all_courses',
+        'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
+      },
+    )
+  end
 
-        results_page.load
-        results_page.study_type_filter.fulltime_checkbox.uncheck
-        results_page.apply_filters_button.click
-      end
+  def when_i_unselect_the_full_time_study_checkbox
+    stub_courses(
+      query: results_page_parameters.merge(
+        'filter[study_type]' => 'part_time',
+      ),
+      course_count: 10,
+    )
 
-      it 'list the filtered courses' do
-        expect(results_page.study_type_filter.legend.text).to eq('Study type')
-        expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(true)
-        expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(false)
-      end
+    results_page.study_type_filter.fulltime_checkbox.uncheck
+  end
 
-      it 'retains the query parameters' do
-        expect_page_to_be_displayed_with_query(
-          page: results_page,
-          expected_query_params: {
-            'parttime' => 'true',
-            'hasvacancies' => 'true',
-            'degree_required' => 'show_all_courses',
-            'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
-          },
-        )
-      end
-    end
+  def and_i_select_the_part_time_study_checkbox
+    results_page.study_type_filter.parttime_checkbox.check
+  end
 
-    context 'show full time and part time courses (default behaviour)' do
-      before do
-        stub_courses(
-          query: base_parameters.merge(
-            'filter[study_type]' => 'part_time,full_time',
-          ),
-          course_count: 10,
-        )
+  def then_i_see_that_the_part_time_study_checkbox_is_still_selected
+    expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(true)
+  end
 
-        results_page.load
-        results_page.study_type_filter.fulltime_checkbox.uncheck
-        results_page.study_type_filter.parttime_checkbox.uncheck
-        results_page.apply_filters_button.click
-      end
+  def and_the_full_time_study_checkbox_is_unselected
+    expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(false)
+  end
 
-      it 'still lists full time and part time courses when both are deselected' do
-        expect(results_page.study_type_filter.legend.text).to eq('Study type')
-        expect(results_page.study_type_filter.parttime_checkbox.checked?).to be(true)
-        expect(results_page.study_type_filter.fulltime_checkbox.checked?).to be(true)
-      end
-
-      it 'retains the query parameters' do
-        expect_page_to_be_displayed_with_query(
-          page: results_page,
-          expected_query_params: {
-            'hasvacancies' => 'true',
-            'degree_required' => 'show_all_courses',
-            'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
-          },
-        )
-      end
-    end
+  def and_the_part_time_study_query_parameters_are_retained
+    expect_page_to_be_displayed_with_query(
+      page: results_page,
+      expected_query_params: {
+        'parttime' => 'true',
+        'hasvacancies' => 'true',
+        'degree_required' => 'show_all_courses',
+        'qualifications' => %w[QtsOnly PgdePgceWithQts Other],
+      },
+    )
   end
 end
