2791209f077a5dd49322ff225487945e7cea0849
Toby Retallick <toby.retallick@gmail.com>
2021-12-06 08:49:46 +0000

    Create feature flag for new search flow

--- app/lib/feature_flags.rb
+++ app/lib/feature_flags.rb
@@ -8,6 +8,7 @@ class FeatureFlags
       [:send_web_requests_to_big_query, 'Send events to Google Big Query', 'Apply team'],
       [:bursaries_and_scholarships_announced, 'Display scholarship and bursary information', 'Apply team'],
       [:componentised_results, 'Refactors the view templates into Rails view components for the results page', 'Apply team'],
+      [:new_search_flow, 'A new search flow which first asks which age you would like to teach', 'Apply team']
     ]
   end
 end
