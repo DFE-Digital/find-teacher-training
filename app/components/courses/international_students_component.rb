96c5d4c467aab411ce07b90a08d43140f1d18133
Frankie Roberto <frankie@frankieroberto.com>
2022-01-06 14:58:05 +0000

    Update content where skilled worker visas can be sponsored

--- app/components/courses/international_students_component.rb
+++ app/components/courses/international_students_component.rb
@@ -3,9 +3,9 @@ module Courses
     include ViewHelper
 
     TRAIN_TO_TEACH_URL = 'https://www.gov.uk/government/publications/train-to-teach-in-england-non-uk-applicants/train-to-teach-in-england-non-uk-applicants#visas-and-immigration'.freeze
-    APPLY_FOR_STUDENT_VISA_URL = 'https://www.gov.uk/government/publications/train-to-teach-in-england-non-uk-applicants/train-to-teach-in-england-if-youre-a-non-uk-citizen#visa'.freeze
     VISA_GUIDANCE_URL = 'https://www.gov.uk/government/publications/train-to-teach-in-england-non-uk-applicants/train-to-teach-in-england-if-youre-a-non-uk-citizen#visa'.freeze
-
+    OTHER_VISA_GUIDANCE_URL = 'https://www.gov.uk/government/publications/train-to-teach-in-england-non-uk-applicants/train-to-teach-in-england-if-youre-a-non-uk-citizen#studying-and-working-as-a-teacher-in-the-uk-without-a-skilled-worker-visa-or-a-student-visa'.freeze
+    
     def initialize(course:)
       @course = course
     end
@@ -18,13 +18,13 @@ module Courses
       
       if !@course.salaried? && provider.can_sponsor_student_visa
         
-        "<p class=\"govuk-body\">You’ll need a visa or other immigration status that allows you to study in the UK. You already have this if you:</p>
+        "<p class=\"govuk-body\">You’ll need a visa or other immigration status that allows you to work in the UK. You already have this if you:</p>
         <ul class=\"govuk-list govuk-list--bullet\">
           <li>are an Irish citizen</li>
           <li>have settled or pre-settled status under the EU Settlement Scheme</li>
         </ul>
         
-        <p class=\"govuk-body\">You may need to #{govuk_link_to('apply for a Student visa', APPLY_FOR_STUDENT_VISA_URL)}.</p>
+        <p class=\"govuk-body\">For courses that come with a salary, this often means #{govuk_link_to('applying for a Skilled Worker visa', VISA_GUIDANCE_URL)}.</p>
 
         <p class=\"govuk-body\">To be able to apply for Student visa, you need a confirmed offer on a course.</p>
         
@@ -32,10 +32,26 @@ module Courses
         
         <p class=\"govuk-body\">Visa sponsorship is available for this course. If you get a place on this course, we’ll help you apply for a Student visa.</p>
         
-        <p class=\"govuk-body\">Alternatively, you may be #{govuk_link_to('eligible for a different type of visa', VISA_GUIDANCE_URL)} that allows you to train to be a teacher without being sponsored.</p>".html_safe                
+        <p class=\"govuk-body\">Alternatively, you may be eligible for other visa types that allow you to #{govuk_link_to('train to be a teacher without a Skilled Worker visa or a Student visa', OTHER_VISA_GUIDANCE_URL)}.</p>".html_safe              
         
       elsif @course.salaried? && provider.can_sponsor_skilled_worker_visa
-        "We can sponsor #{govuk_link_to('Skilled Worker visas', TRAIN_TO_TEACH_URL)}, but this is not guaranteed.".html_safe
+        
+        "<p class=\"govuk-body\">You’ll need a visa or other immigration status that allows you to study in the UK. You already have this if you:</p>
+        <ul class=\"govuk-list govuk-list--bullet\">
+          <li>are an Irish citizen</li>
+          <li>have settled or pre-settled status under the EU Settlement Scheme</li>
+        </ul>
+        
+        <p class=\"govuk-body\">For courses that come with a salary, this often means #{govuk_link_to('applying for a Skilled Worker visa', VISA_GUIDANCE_URL)}.</p>
+        
+        <p class=\"govuk-body\">To be able to apply for a Skilled Worker visa, you need a confirmed offer on a salaried teacher training course. This needs to include employment at a school which has an employer sponsor licence.</p>
+        
+        <p class=\"govuk-body\">To get a confirmed offer, you need to pass criminal record and health checks.</p>
+        
+        <p class=\"govuk-body\">Get in touch to find out if we can help you organise a Skilled Worker visa.</p>
+        
+        <p class=\"govuk-body\">Alternatively, you may be eligible for other visa types that allow you to #{govuk_link_to('train to be a teacher without a Skilled Worker visa or a Student visa', OTHER_VISA_GUIDANCE_URL)}.</p>".html_safe
+        
       else
         "We cannot sponsor visas. You’ll need to #{govuk_link_to('get the right visa or status to study in the UK', TRAIN_TO_TEACH_URL)}.".html_safe
       end
