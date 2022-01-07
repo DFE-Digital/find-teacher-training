da59a982a495b327fd972789121f9020123df9fb
EmmaFrith <56349171+EmmaFrith@users.noreply.github.com>
2022-01-07 16:17:44 +0000

    Update app/components/courses/international_students_component.rb

--- app/components/courses/international_students_component.rb
+++ app/components/courses/international_students_component.rb
@@ -22,7 +22,7 @@ module Courses
           <li>have settled or pre-settled status under the EU Settlement Scheme</li>
         </ul>
 
-        <p class=\"govuk-body\">If you do not already have the right to study in the UK, you may need to apply for a Student visa. Student visa sponsorship is available for this course.</p>
+        <p class=\"govuk-body\">If you do not already have the right to study in the UK, you may need to apply for a Student visa. Student visa sponsorship is available for this course. If you get a place on this course, we’ll help you apply for your visa.</p>
 
         <p class=\"govuk-body\">If you get a place on this course, we’ll help you apply for a Student visa.</p>
 
