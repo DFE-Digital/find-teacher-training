0d0de71560f2ecb6a1aa0715a8e7f1c0b832afa4
EmmaFrith <56349171+EmmaFrith@users.noreply.github.com>
2022-01-10 18:22:18 +0000

    Update app/components/courses/international_students_component.rb

--- app/components/courses/international_students_component.rb
+++ app/components/courses/international_students_component.rb
@@ -34,7 +34,7 @@ module Courses
           <li>have settled or pre-settled status under the EU Settlement Scheme</li>
         </ul>
 
-        <p class=\"govuk-body\">If you do not already have the right to work in the UK, you may need to apply for a Skilled Worker visa. Skilled Worker visa sponsorship is available for this course. If you get a place on this course, we’ll help you apply for your visa.</p>
+        <p class=\"govuk-body\">If you do not already have the right to work in the UK for the duration of this course, you may need to apply for a Skilled Worker visa. Skilled Worker visa sponsorship is available for this course. If you get a place on this course, we’ll help you apply for your visa.</p>
 
 
         <p class=\"govuk-body\">Alternatively, you may be eligible for visa types that allow you to train to be a teacher without a Skilled Worker visa.</p>
