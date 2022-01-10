0f9818d141f187a472c4307cb7e278f1ef0fcdc3
EmmaFrith <56349171+EmmaFrith@users.noreply.github.com>
2022-01-10 16:15:05 +0000

    Update app/components/courses/international_students_component.rb

--- app/components/courses/international_students_component.rb
+++ app/components/courses/international_students_component.rb
@@ -26,7 +26,7 @@ module Courses
 
 
         <p class=\"govuk-body\">Alternatively, you may be eligible for visa types that allow you to train to be a teacher without a Student visa.</p>
-        <p class=\"govuk-body\">Find out more about visas and immigration statuses.</p>".html_safe
+<p class=\"govuk-body\">#{govuk_link_to('Find out more about visas and immigration statuses', VISA_GUIDANCE_URL)}.</p>.html_safe
       elsif @course.salaried? && provider.can_sponsor_skilled_worker_visa
         "<p class=\"govuk-body\">You’ll need the right to work in the UK. You already have this if you:</p>
         <ul class=\"govuk-list govuk-list--bullet\">
