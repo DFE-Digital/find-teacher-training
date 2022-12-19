71804ba8225972087e9eef082c8bb1984e585538
defong <de_fong2k@yahoo.com>
2022-12-19 14:47:44 +0000

    [0892] Added course level for secondary courses

--- app/components/courses/summary_component.rb
+++ app/components/courses/summary_component.rb
@@ -11,10 +11,20 @@ module Courses
              :length,
              :applications_open_from,
              :outcome,
-             :start_date, to: :course
+             :start_date,
+             :secondary_course?,
+             :level, to: :course
 
     def initialize(course)
       @course = course
+    end
+
+    def age_range_in_years_row
+      if secondary_course?
+        "#{age_range_in_years.humanize} - #{level}"
+      else
+        age_range_in_years.humanize
+      end
     end
   end
 end
